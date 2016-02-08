3da64f95de343fee26e7b71794ff4b99fc492b25
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-08 21:18:51 +0530

    Added Addon Initiato

---
+++ lib/Initiator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace xepan\commerce;
+
+class Initiator extends \Controller_Addon {
+	public $addon_name = 'xepan_commerce';
+
+	function init(){
+		parent::init();
+		$this->routePages('xepan_commerce');
+		
+	}
+}
