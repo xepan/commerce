4e07676630b8bde30d6971cc8591e8870bda1f32
deepak <dkanojia93@gmail.com>
2016-02-18 17:31:04 +0530

    done

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -8,6 +8,15 @@ class Initiator extends \Controller_Addon {
 	function init(){
 		parent::init();
 		$this->routePages('xepan_commerce');
+		$this->addLocation(array('template'=>'templates'));
 		
+		$m = $this->app->top_menu->addMenu('Commerce');
+		$m->addItem('Item','xepan_commerce_itemlist');
+		$m->addItem('Customer','xepan_commerce_customerprofile');
+		$m->addItem('Supplier','xepan_commerce_supplier');
+		$m->addItem('Sales','xepan_commerce_salesorder');
+		$m->addItem('SalesInvoice','xepan_commerce_salesinvoice');
+		$m->addItem('Purchase','xepan_commerce_purchaseorder');
+		$m->addItem('PurchaseInvoice','xepan_commerce_purchaseinvoice');
 	}
 }
