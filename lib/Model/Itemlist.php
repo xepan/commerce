bd3dd379226f77cd3bb514af3d39100822f577b7
deepak <dkanojia93@gmail.com>
2016-02-22 12:38:07 +0530

    done

--- lib/Model/Itemlist.php
+++ lib/Model/Itemlist.php
@@ -12,17 +12,8 @@
 		// Basic Field
 		$this->addField('name')->mandatory(true);
 		$this->addField('code')->mandatory(true);
-		// $this->addField('is_publis')->type('boolean')->defaultValue(true);
-		// $this->addField('is_party_publish')->type('boolean')->defaultValue(false);
-
 		$this->addField('original_price')->type('money')->mandatory(true);
 		$this->addField('sales_price')->type('money')->mandatory(true);
-		// // $this->addField('short_description')->type('text');
-		
-		// // $this->addField('rank_weight')->defaultValue(0)->hint('Higher Rank Weight Item Display First')->mandatory(true);
-		// // $this->addField('expiry_date')->type('date');
-		// // $this->addField('description')->type('text');
-
 		$this->addField('status')->enum(['Active','Inactive']);
 		$this->addField('total_sale')->mandatory(true);
 	}
