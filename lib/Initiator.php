8d21701e61dd4e12ce0acb73a0843f591491aeb5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-01 09:16:56 +0530

    Modefied to work with frontend

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -10,14 +10,16 @@ class Initiator extends \Controller_Addon {
 		$this->routePages('xepan_commerce');
 		$this->addLocation(array('template'=>'templates'));
 		
-		$m = $this->app->top_menu->addMenu('Commerce');
-		$m->addItem('Item','xepan_commerce_item');
-		$m->addItem('Customer','xepan_commerce_customer');
-		$m->addItem('Supplier','xepan_commerce_supplier');
-		$m->addItem('Sales','xepan_commerce_salesorder');
-		$m->addItem('SalesInvoice','xepan_commerce_salesinvoice');
-		$m->addItem('Purchase','xepan_commerce_purchaseorder');
-		$m->addItem('PurchaseInvoice','xepan_commerce_purchaseinvoice');
-		$m->addItem('Quotation','xepan_commerce_quotation');
+		if($this->app->is_admin){
+			$m = $this->app->top_menu->addMenu('Commerce');
+			$m->addItem('Item','xepan_commerce_item');
+			$m->addItem('Customer','xepan_commerce_customer');
+			$m->addItem('Supplier','xepan_commerce_supplier');
+			$m->addItem('Sales','xepan_commerce_salesorder');
+			$m->addItem('SalesInvoice','xepan_commerce_salesinvoice');
+			$m->addItem('Purchase','xepan_commerce_purchaseorder');
+			$m->addItem('PurchaseInvoice','xepan_commerce_purchaseinvoice');
+			$m->addItem('Quotation','xepan_commerce_quotation');
+		}
 	}
 }
