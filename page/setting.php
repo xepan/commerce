6178b542f54e22eb2c93bfb886bcaff2aa7fefb4
vijay mali <guddavijay@gmail.com>
2016-03-04 19:45:20 +0530

    Currency Model Created & relation with customer And extends Hr Model Document

---
+++ page/setting.php
@@ -0,0 +1,15 @@
+<?php
+namespace xepan\commerce;
+class page_setting extends \Page{
+	public $title="General Settings";
+	function init(){
+		parent::init();
+
+		$crud=$this->add('xepan\hr\CRUD',null,'currency_view',['view/setting/currency-grid']);
+		$crud->setModel('xepan\commerce\Currency',['icon','name','value','status']);
+
+	}
+	function defaultTemplate(){
+		return ['page/setting'];
+	}
+}
