d171bafcb60f008db2e0e378358267d2cbbe82b3
vaibhav <vaibhavsharma38@gmail.com>
2016-03-07 19:21:15 +0530

    trying hands on salesorder !

--- page/salesorderdetail.php
+++ page/salesorderdetail.php
@@ -21,26 +21,9 @@ class page_salesorderdetail extends \Page {
 		$sorder= $this->add('xepan\commerce\Model_Order_SalesOrder')->tryLoadBy('id',$this->api->stickyGET('document_id'));
 		
 		$sinvoice_no = $this->add('xepan\base\View_Document',['action'=>$action],null,['page/order/sales/invoice']);
-		$sinvoice_no->setModel($sorder,	['name','created_at','delivery_date',
-
-										'billing_address','billing_city',
-										'billing_state','billing_country','billing_zip',
-
-										'shipping_address','shipping_city',
-										'shipping_state','shipping_country','shipping_zip',
-
-										'discount_voucher_amount','gross_amount','total_amount','net_amount'],
-										
-										['name','created_at_id','delivery_date_id',
-
-										'billing_address','billing_city',
-										'billing_state','billing_country','billing_zip',
-										
-										'shipping_address','shipping_city',
-										'shipping_state','shipping_country','shipping_zip',
-
-										'discount_voucher_amount','gross_amount','total_amount','net_amount']);
 		$sinvoice_no->setIdField('document_id');
+
+		$sinvoice_no->setModel($sorder,['name','created_at','discount_amount','gross_amount','total_amount','net_amount'],['qt_no','created_at','discount_amount']);
 
 	}
 }
