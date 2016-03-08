f02ea93565e2aa0ddace1c99377adb8f342cca0f
vijay mali <guddavijay@gmail.com>
2016-03-08 12:28:50 +0530

    Purchase Order extends QSP master Model

--- lib/Model/Order/PurchaseOrder.php
+++ lib/Model/Order/PurchaseOrder.php
@@ -2,7 +2,7 @@
 
 namespace xepan\commerce;
 
-class Model_Order_PurchaseOrder extends \xepan\commerce\Model_Order{
+class Model_Order_PurchaseOrder extends \xepan\commerce\Model_QSP_Master{
 	function init(){
 		parent::init();
 		$this->addCondition('type','PurchaseOrder');
