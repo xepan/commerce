81d1f89b45e450dd25ee70991c9c3cf1a68bf2bc
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-11 00:12:23 +0530

    BUG

--- page/customerdetail.php
+++ page/customerdetail.php
@@ -24,6 +24,7 @@ class page_customerdetail extends \Page {
 		$contact_view->setModel($customer);
 
 		$d = $this->add('xepan\base\View_Document',['action'=>$action],'basic_info',['page/customer/detail','basic_info']);
+		$d->setIdField('contact_id');
 		$d->setModel($customer,['shipping_address','shipping_city','shipping_state','shipping_country','shipping_pincode',
 								'billing_address','billing_city','billing_state','billing_country','billing_pincode','tin_no','pan_no','organization','currency'],
 								['shipping_address','shipping_city','shipping_state','shipping_country','shipping_pincode',
