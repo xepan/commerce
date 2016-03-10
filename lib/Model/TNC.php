4dddb605c78bdaffc2de2a30bdcd3bb41ea4056c
deepak <dkanojia93@gmail.com>
2016-03-10 16:00:38 +0530

    relate with qsp mater with hasmany

--- lib/Model/TNC.php
+++ lib/Model/TNC.php
@@ -2,7 +2,7 @@
 
  namespace xepan\commerce;
 
- class Model_TNC extends \xepan\commerce\Model_Document{
+ class Model_TNC extends \xepan\hr\Model_Document{
  	public $actions = ['*'=>'view','edit','delete'];
 	function init(){
 		parent::init();
@@ -11,7 +11,7 @@
 		$document_j->addField('name');
 		$document_j->addField('content')->type('text');
 
-		$document_j->hasMany('xepan/commerce/Quotation','tnc_id');
+		$document_j->hasMany('xepan/commerce/QSP_Master','tnc_id');
 	}
 }
  
