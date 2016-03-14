6f2cf6f3de3aa85b9b4f49853065477e2707cc21
deepak <dkanojia93@gmail.com>
2016-03-14 13:37:44 +0530

    Document Id Field has been addedAll QSP pages grid

--- page/purchaseinvoicedetail.php
+++ page/purchaseinvoicedetail.php
@@ -92,7 +92,7 @@
 		
 		$dv = $this->add('xepan\commerce\View_QSPAddressJS')->set('');
 
-		$view=$this->add('xepan\commerce\View_QSP',['qsp_model'=>$purchase_inv_dtl,'qsp_view_field'=>$view_field,'qsp_form_field'=>$form_field]);
+		$view = $this->add('xepan\commerce\View_QSP',['qsp_model'=>$purchase_inv_dtl,'qsp_view_field'=>$view_field,'qsp_form_field'=>$form_field]);
 
 		$contact_field = $view->document->form->getElement('contact_id');
 		$contact_field->js('change',$dv->js()->reload(['changed_contact_id'=>$contact_field->js()->val()]));
