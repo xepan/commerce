2d9d0e97b225d007f1be06c09cba1fc68e51b50f
vaibhav <vaibhavsharma38@gmail.com>
2016-03-16 13:33:18 +0530

    itemimage with elevate zoom.

--- lib/Tool/ItemImage.php
+++ lib/Tool/ItemImage.php
@@ -10,8 +10,27 @@ class Tool_ItemImage extends \xepan\base\View_Tool{
 		parent::init();
 
 		$item_id = $_GET['commerce_item_id'];		
-		$image = $this->add('xepan\commerce\Model_Item')->load($item_id)->ref('Attachments');
+		$item = $this->add('xepan\commerce\Model_Item')->load($item_id);
+		$image = $item->ref('Attachments');
+		// $image->addExpression('thumb_url')->set(function($m,$q){
+		// 	$file = $this->add('filestore\Field_Image')->load($m['file_id']);
 
-		$this->add('Lister',null,null,['view/tool/itemimage'])->setModel($image);
+		// 	return $file->;
+		// });
+
+		$lister = $this->add('CompleteLister',null,null,['view/tool/itemimage']);
+		$lister->setModel($image);
+
+		//For set First Image
+		$lister->template->set('firstimage',$this->add('xepan\commerce\Model_Item')->load($item_id)->ref('Attachments')->setLimit(1)->fieldQuery('file'));
+		
+	}
+
+	function render(){
+
+		$this->js(true)->_load('tool/jquery-elevatezoom')
+					->_load('tool/jquery.fancybox');
+		parent::render();
+
 	}
 }	
