d6708e0b22ff03a0b1381aba9aa10da501facae4
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-16 17:38:25 +0530

    Updated cart modela dn tool

--- lib/Model/Cart.php
+++ lib/Model/Cart.php
@@ -15,11 +15,13 @@
 		
 		$this->addField('unit_price');
 		$this->addField('qty');
-		$this->add('misc/Field_Callback','amount_excluding_tax')->set(function($m){
-			return round($m['unit_price'] * $m['qty'], 2);
-		});
 		$this->addField('tax_percentage');
 		$this->addField('shipping_charge');
+
+		$this->addHook('afterLoad',function($m){
+			$m['amount_excluding_tax']=number_format($m['unit_price'] * $m['qty'],2);
+			$m['tax_amount']=number_format($m['amount_excluding_tax']*$m['tax_percentage']/100.00,2);
+		});
 
 	}
 
@@ -34,10 +36,10 @@
 		$this['type'] = $data['type'];
 		$this['shipping_charge'] = $data['shipping_charge'];
 		$this['unit_price'] = $data['unit_price'];
-		$this['tax_percantage'] = $data['tax_percantage'];
+		$this['tax_percentage'] = $data['tax_percentage'];
 		$this['amount_excluding_tax'] = $data['amount_excluding_tax'];
 		$this['total_amount'] = $data['total_amount'];
-		$this['tax_amount'] = $data['tax_amount'];
+		// $this['tax_amount'] = $data['tax_amount'];
 
 		$this->save();
 
--- lib/Tool/Cart.php
+++ lib/Tool/Cart.php
@@ -15,6 +15,17 @@ class Tool_Cart extends \xepan\cms\View_Tool{
 		$lister=$this->add('CompleteLister',null,null,['view/tool/cart']);
 		$lister->setModel($cart);
 
+		$sum_amount_excluding_tax=0;
+		$sum_tax_amount=0;
+
+		foreach ($cart as $item) {
+			$sum_amount_excluding_tax += $item['amount_excluding_tax'];
+			$sum_tax_amount += $item['tax_amount'];
+		}
+		
+		$lister->template->set('sum_amount_excluding_tax',$sum_amount_excluding_tax);
+		$lister->template->set('tax_amount',$sum_tax_amount);
+
 	}
 
 	// function defaultTemplate(){
