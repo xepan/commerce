9533ca222c3aa6ce6aa1856d1b9f73fd1123f59c
vijay mali <guddavijay@gmail.com>
2016-03-26 13:55:02 +0530

    replace billing_tel to billing_contact field name

--- lib/View/QSPAddressJS.php
+++ lib/View/QSPAddressJS.php
@@ -17,7 +17,7 @@ class View_QSPAddressJS extends \View {
 			$js[] = $this->js()->_selector('.billing_state')->find('input')->val($contact['billing_state']?:$contact['state']);
 			$js[] = $this->js()->_selector('.billing_country')->find('input')->val($contact['billing_country']?:$contact['country']);
 			$js[] = $this->js()->_selector('.billing_pincode')->find('input')->val($contact['billing_city']?:$contact['pin_code']);
-			$js[] = $this->js()->_selector('.billing_tel')->find('input')->val($contact['billing_tel']?:$contact->ref('Phones')->tryLoadAny()->get('value'));
+			$js[] = $this->js()->_selector('.billing_tel')->find('input')->val($contact['billing_contact']?:$contact->ref('Phones')->tryLoadAny()->get('value'));
 			$js[] = $this->js()->_selector('.billing_email')->find('input')->val($contact['billing_email']?:$contact->ref('Emails')->tryLoadAny()->get('value'));
 			
 			// shipping address
@@ -26,7 +26,7 @@ class View_QSPAddressJS extends \View {
 			$js[] = $this->js()->_selector('.shipping_state')->find('input')->val($contact['shipping_state']?:$contact['state']);
 			$js[] = $this->js()->_selector('.shipping_country')->find('input')->val($contact['shipping_country']?:$contact['country']);
 			$js[] = $this->js()->_selector('.shipping_pincode')->find('input')->val($contact['shipping_city']?:$contact['pin_code']);
-			$js[] = $this->js()->_selector('.shipping_tel')->find('input')->val($contact['shipping_tel']?:$contact->ref('Phones')->tryLoadAny()->get('value'));
+			$js[] = $this->js()->_selector('.shipping_tel')->find('input')->val($contact['shipping_contact']?:$contact->ref('Phones')->tryLoadAny()->get('value'));
 			$js[] = $this->js()->_selector('.shipping_email')->find('input')->val($contact['shipping_email']?:$contact->ref('Emails')->tryLoadAny()->get('value'));
 
 			if(in_array($contact['type'],['Customer','Supplier'])){
