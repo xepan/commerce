37bf7fce34b3789073904717c0e0b2b718dff4c3
vijay mali <guddavijay@gmail.com>
2016-03-26 22:35:08 +0530

    conflict resolved

--- lib/Model/QSP/Detail.php
+++ lib/Model/QSP/Detail.php
@@ -29,6 +29,8 @@ class Model_QSP_Detail extends \xepan\base\Model_Table{
 		$this->addField('narration');
 		$this->addField('extra_info')->type('text'); // Custom Fields
 
+		$this->addExpression('order_contact')->set($this->refSQL('qsp_master_id')->fieldQuery('contact'));
+
 		//has many departmental status
 		$this->hasMany('xepan\commerce\OrderItemDepartmentalStatus','qsp_detail_id');
 
