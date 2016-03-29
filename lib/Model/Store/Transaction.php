d80993816d63e91f431b6821f3adde2b58b37f92
vijay mali <guddavijay@gmail.com>
2016-03-29 16:35:13 +0530

    Renamed Store Model

---
+++ lib/Model/Store/Transaction.php
@@ -0,0 +1,44 @@
+<?php
+namespace xepan\commerce;
+class Model_Store_Transaction extends \xepan\base\Model_Table{
+	public $table="store_transaction";
+	public $acl=false;
+	function init(){
+		parent::init();
+
+		$this->hasOne('xepan\base\Epan','epan_id');
+		$this->hasOne('xepan\commerce\Store_Warehouse','from_warehouse_id');
+		$this->hasOne('xepan\commerce\Store_Warehouse','to_warehouse_id');
+		
+		$this->addField('related_document_id');
+		$this->addField('document_type');
+
+		$this->hasMany('xepan\commerce\Store_TransactionRow','store_transaction_id',null,'StoreTransactionRows');
+	}
+	
+	function fromWarehouse($warehouse=false){
+		if($warehouse)
+			$this['from_warehouse_id'] = $warehouse->id;
+		else
+			return $this->ref('from_warehouse_id');
+	}
+
+	function toWarehouse($warehouse=false){
+		if($warehouse)
+			$this['to_warehouse_id'] = $warehouse->id;
+		else
+			return $this->ref('to_warehouse_id');
+	}
+
+	function addItem($item_id,$qty,$custom_fields,$customfield_value){
+		$new_item = $this->ref('StoreTransactionRows');
+		$new_item['store_transaction_id'] = $this->id;
+		$new_item['item_id'] = $item_id;
+		$new_item['qty'] = $qty;
+		$new_item['customfield_generic_id'] = $custom_fields;
+		$new_item['customfield_value_id'] = $customfield_value ;
+		$new_item->save();
+
+		return $this;
+	}
+}
