9c42a134b3696c214e27c18fda77aef6e955e22b
vijay mali <guddavijay@gmail.com>
2016-03-29 16:37:38 +0530

    Warehouse to created new tranasction & rows

--- page/store/transaction.php
+++ page/store/transaction.php
@@ -1,10 +1,11 @@
 <?php
 namespace xepan\commerce;
+
 class page_store_transaction extends \Page{
 	public $title="Store Warehouse";
 	function init(){
 		parent::init();
 
-		$this->add('xepan\hr\CRUD')->setModel('xepan\commerce\Store_StockTransaction');
+		$this->add('xepan\hr\CRUD',null,null,['view/store/transaction-grid'])->setModel('xepan\commerce\Store_Transaction');
 	}
 }
