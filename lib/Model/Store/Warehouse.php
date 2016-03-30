03f432975317e7c2277d1fb625ac4dc12bbacf31
vijay mali <guddavijay@gmail.com>
2016-03-30 16:59:00 +0530

    Function newTransaction modified

--- lib/Model/Store/Warehouse.php
+++ lib/Model/Store/Warehouse.php
@@ -14,13 +14,17 @@ class Model_Store_Warehouse extends \xepan\base\Model_Contact{
 	}
 
 
-	function newTransaction($related_document_id,$related_doc_contact_id=null,$related_document_type){
+	function newTransaction($related_document_id,$jobcard,$related_doc_contact_id=null,$related_document_type){
 
+		// throw new \Exception($jobcard->id);
+		
 		$m = $this->add('xepan\commerce\Model_Store_Transaction');
-		$m['document_type'] = $related_document_type;
+		$m['type'] = $related_document_type;
 		$m['from_warehouse_id'] = $related_doc_contact_id;
 		$m['to_warehouse_id'] = $this->id;
 		$m['related_document_id']=$related_document_id	;	
+		$m['jobcard_id']=$jobcard->id;	
+		$m['status']='ToReceived';	
 		$m->save();
 		return $m;
 	}
