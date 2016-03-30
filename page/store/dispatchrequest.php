32ff2c32f6928d40dbc8349df8aed68e361ab5e3
vijay mali <guddavijay@gmail.com>
2016-03-30 19:04:23 +0530

    Create new files

--- page/store/dispatchrequest.php
+++ page/store/dispatchrequest.php
@@ -2,14 +2,14 @@
 
 namespace xepan\commerce;
 
-class page_store_dispatch extends \Page{
-	public $title="Dispatch Item";
+class page_store_dispatchrequest extends \Page{
+	public $title="Dispatch Request";
 	function init(){
 		parent::init();
 
 		$dispatch=$this->add('xepan\commerce\Model_Store_DispatchRequest');
 
-		$c=$this->add('xepan\hr\CRUD',null,null,['view/store/dispatch-grid']);
+		$c=$this->add('xepan\hr\CRUD',null,null,['view/store/dispatch-request-grid']);
 		$c->setModel($dispatch);
 	}
 }
