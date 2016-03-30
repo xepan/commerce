986076cad87c13e11c3b9e59cd45117a3b9e14cd
vijay mali <guddavijay@gmail.com>
2016-03-30 19:49:33 +0530

    send item to dispatch partial complete

--- page/store/deliveryManagment.php
+++ page/store/deliveryManagment.php
@@ -19,21 +19,44 @@ class page_store_deliveryManagment extends \Page{
 		$tra_j=$tra_row->join('store_transaction.id');
 		$tra_j->addField('related_document_id');
 		$tra_j->addField('jobcard_id');
+		$tra_j->addField('status');
 		$tra_row->_dsql()->group('related_document_id');
 
-		$this->add('H3')->set('Items to Deliver');
+		// $this->add('H3')->set('Items to Deliver');
 
-		$grid = $this->add('xepan\hr\Grid',null,null,['view/store/deliver-grid']);
+		$grid = $this->add('xepan\hr\Grid',null,'send',['view/store/deliver-grid']);
 		$grid->setModel($tra_row);
+		$grid->template->tryDel('Pannel');
 
-		$f=$this->add('Form');
+
+		$f=$this->add('Form',null,'send');
 
 		$select_item = $f->addField('hidden','select_item');
 
 		$grid->addSelectable($select_item);
-		$f->add('View')->setElement('br');
 
 		$f->addSubmit('Dispatch the Order');
 
+		if($f->isSubmitted()){
+			if(!$f['select_item'])
+				throw $f->displayError($f['select_item'],'No Item Selected');
+
+			$orderitems_selected = array();
+			$items_selected = json_decode($f['select_item'],true);
+
+			foreach ($items_selected as  $value) {
+				$item = $this->add('xepan\commerce\Model_Store_TransactionRow')->load($value);
+				$orderitems_selected[] = $item['qsp_detail_id'];
+			}
+
+			$this['status']="Dispatch";
+			$this->save();		
+
+			$f->js(null,$f->js()->univ()->successMessage('SuccessFully Dispatch'))->reload()->execute();	
+		}
+	}
+
+	function defaultTemplate(){
+		return['page/store/delivery-managment'];
 	}
 }
