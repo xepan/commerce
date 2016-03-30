20a07a40dcd1aec4129b761a5f914b3e0c61ba3d
vijay mali <guddavijay@gmail.com>
2016-03-30 14:34:57 +0530

    Warehouse newTrasaction function parameters passed to used any where

--- lib/Model/Store/Warehouse.php
+++ lib/Model/Store/Warehouse.php
@@ -14,13 +14,13 @@ class Model_Store_Warehouse extends \xepan\base\Model_Contact{
 	}
 
 
-	function newTransaction($related_document,$related_document_type){
+	function newTransaction($related_document_id,$related_doc_contact_id=null,$related_document_type){
 
 		$m = $this->add('xepan\commerce\Model_Store_Transaction');
 		$m['document_type'] = $related_document_type;
-		$m['from_warehouse_id'] = $related_document['contact_id'];
+		$m['from_warehouse_id'] = $related_doc_contact_id;
 		$m['to_warehouse_id'] = $this->id;
-		$m['related_document_id']=$related_document->id;	
+		$m['related_document_id']=$related_document_id	;	
 		$m->save();
 		return $m;
 	}
