5124cf985324b95f748b792e8a82bbfb2a6ba296
vijay mali <guddavijay@gmail.com>
2016-03-31 12:50:42 +0530

    Dispatch request modified

--- page/store/dispatchrequest.php
+++ page/store/dispatchrequest.php
@@ -7,8 +7,8 @@ class page_store_dispatchrequest extends \Page{
 	function init(){
 		parent::init();
 
-		$dispatch=$this->add('xepan\commerce\Model_Store_DispatchRequest');
-
+		$dispatch=$this->add('xepan\commerce\Model_Store_Transaction');	
+		$dispatch->addCondition('type','Dispatch');
 		$c=$this->add('xepan\hr\CRUD',null,null,['view/store/dispatch-request-grid']);
 		$c->setModel($dispatch);
 	}
