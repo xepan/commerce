c6db30d3fde031729ca04b1074d22844c90e94fc
vijay mali <guddavijay@gmail.com>
2016-03-31 14:19:01 +0530

    received status function copy to parent class

--- lib/Model/Store/Transaction.php
+++ lib/Model/Store/Transaction.php
@@ -58,5 +58,25 @@ class Model_Store_Transaction extends \xepan\base\Model_Table{
 	}
 
 	function receive(){
+		$tra_row=$this->add('xepan\commerce\Model_Store_TransactionRow');
+		$tra_row->addCondition('store_transaction_id',$this->id);
+		$tra_row->tryLoadAny();
+		// throw new \Exception($tra_row->id, 1);
+		
+		$job_detail=$this->add('xepan\production\Model_Jobcard_Detail');
+		$job_detail->addCondition('id',$tra_row['jobcard_detail_id']);
+		$job_detail->tryLoadAny();
+
+		if($job_detail->loaded()){
+			$job_detail['status']="Completed";
+			$job_detail->save();
+
+			$this['status']="Received";
+			$this->save();
+			return true;
+		}else{
+			throw new \Exception("Model Job Detail Must be Loaded", 1);
+		}
+		
 	}
 }
