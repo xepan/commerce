b89cd1d94667734eb5c74dde5b3f831b5d3f7524
sinharakesh <rksinha.btech@gmail.com>
2016-04-06 12:28:45 +0530

    category tool custom link added

--- lib/Tool/Category.php
+++ lib/Tool/Category.php
@@ -6,7 +6,7 @@ class Tool_Category extends \xepan\cms\View_Tool{
 	public $options = [
 				'show_name'=>1,
 				'layout'=>'vertical',
-				'show_description' =>0,
+				'show_description' =>false,
 				'show_price' =>1,
 				'grid-column' =>12,
 				'show-category-column'=>1,
@@ -17,13 +17,8 @@ class Tool_Category extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 		$categories = $this->add('xepan\commerce\Model_Category');
-		
-		if($this->options['show_description']){
-			$cat_m = $categories->load($_GET['xsnb_category_id']);
-			// $content = str_replace("{{category_id}}", $_GET['xsnb_category_id'], $cat_m['description']);
-			// $content = str_replace("{{product_page_name}}",$this->html_attributes['xshop_category_url_page'] , $content);
-			// $this->add('View')->setHTML($content);
-		}
+		$this->add('xepan\cms\Controller_Tool_Optionhelper',['model'=>$categories]);
+
 		if($this->options['show_name']){
 			$cat_model=$this->add('xepan\commerce\Model_Category');
 			$cat_name = $cat_model->get('name');
@@ -74,7 +69,7 @@ class Tool_Category extends \xepan\cms\View_Tool{
 			$output ="<div class='body epan-sortable-component epan-component  ui-sortable ui-selected'>";
 			$output ="<ul class='sky-mega-menu sky-mega-menu-anim-slide sky-mega-menu-response-to-stack'>";
 			foreach ($categories as $junk_category) {
-				$output .= $this->getCategory($categories,$categories['custom_link']?$categories['custom_link']:$this->options['url_page']);
+				$output .= $this->getCategory($categories);
 			}
 			$output.="</ul></div>";
 			$this->setHTML($output);
@@ -93,32 +88,29 @@ class Tool_Category extends \xepan\cms\View_Tool{
 		// $item->setOrder('sale_price','asc');
 
 		$item->tryLoadAny();
+		$url = $category['custom_link']?$category['custom_link']:$this->options['url_page'];
 
 		if($category->ref('SubCategories')->count()->getOne() > 0){
 			$sub_category = $category->ref('SubCategories')->addCondition('status','Active');
 			$output = "<li aria-haspopup='true' class='xshop-category'>";
-			$output .="<a href='".$this->api->url(null,array('subpage'=>$this->options['url_page'],'xsnb_category_id'=>$category->id))."'>";
+			$output .="<a href='".$this->api->url($url,array('xsnb_category_id'=>$category->id))."'>";
 			$output .= $category['name'];
 			$output .="</a>" ;
 			$output .= "<div class='grid-container3'>";
 			$output .= "<ul>";
 			foreach ($sub_category as $junk_category) {
-				$output .= $this->getCategory($sub_category,$category['custom_link']?$category['custom_link']:$this->options['url_page']);
+				$output .= $this->getCategory($sub_category);
 			}
 			$output .= "</ul>";
 			$output .= "</div>";
 			$output .= "</li>";
 
-			if($this->options['category-add-pin']){
-				$output .= '<li class="xshop-pin-category pull-right"><a class="icon-pin" onclick="swapfixed(event);">'.$this->options["category-pin-label"].'</a>';
-				$output .= "</li>";
-			}
 		}else{
 			// throw new \Exception($category['id'], 1);
 			if($this->options['layout']){
 				$output = "<li class='text-center ".$this->col."'><a href='".$this->api->url(null,array('subpage'=>$this->options['url_page'],'xsnb_category_id'=>$category->id))."'><div class='sky-menu-thumbnail-name'>".$category['name']."</div></a></li>";
 			}else{
-				$output = "<li><a href='".$this->api->url(null,array('subpage'=>$page_name,'xsnb_category_id'=>$category->id))."'>".$category['name'];
+				$output = "<li><a href='".$this->api->url($url,array('xsnb_category_id'=>$category->id))."'>".$category['name'];
  				if($this->options['show_price'])
 					$output.= " " . $item['sale_price'];
 				$output.="</a></li>";
