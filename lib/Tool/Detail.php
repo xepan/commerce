fa0dcd105671f6f4c39a010cc45d2531956bdb71
dkanojia <dkanojia93@gmail.com>
2016-04-09 17:05:08 +0530

    Specification jade Added and Detail Tool in working.

--- lib/Tool/Detail.php
+++ lib/Tool/Detail.php
@@ -5,32 +5,35 @@ namespace xepan\commerce;
 class Tool_Detail extends \xepan\cms\View_Tool{
 	public $options = [
 				// 'display_layout'=>'item-description',/*flat*/
+					 
 				];
 
 	function init(){
 		parent::init();
 
-		// $item_id = $_GET['commerce_item_id'];
-		$item = $this->add('xepan\commerce\Model_Item');
-			///throw $this->exception('Item not found');
-		$this->setModel($item)->tryLoadAny();
+		$item_id = $_GET['commerce_item_id'];		
+		$item = $this->add('xepan\commerce\Model_Item')->load($item_id);
 
-		// $options = [
-		// 			'button_name'=>$this->options['addtocart_name']
-		// 			];
+		$this->template->set('name',$item['name']);
+		$this->template->set('description',$item['description']);
 
+		$spf = $this->add('xepan\commerce\Model_Item_CustomField_Association')
+		->addCondition('item_id',$item->id);
+		$grid_s = $this->add('xepan\base\Grid',null,'specification',['view/tool/specification']);
+		$grid_s->setModel($spf);
 
-		// $cart = $this->add('xepan\commerce\Tool_Item_AddToCartButton',
-		// 	[
-		// 		'name' => "addtocart_view_".$l->model->id,
-		// 		'options'=>$options
-		// 	],
-		// 	''
-		// 	);
+		$options = ['button_name'=>$this->options['addtocart_name']];
+		
+		$cart_btn = $this->add('xepan\commerce\Tool_Item_AddToCartButton',
+					['options'=>$options],
+					'Addtocart');
+		$cart_btn->setModel($item);
+		$this->template->set['Addtocart'] = $cart_btn->getHtml();
+
 	}
 
 	function defaultTemplate(){
 		return ['view/tool/'.$this->options['layout']];
 	}
-
+	
 }
---
+++ lib/View/Tool.php
@@ -0,0 +1,70 @@
+<?php
+ namespace xepan\commerce;
+class View_QSP extends \View{
+
+	public $qsp_model;
+	public $qsp_view_field = ['x'];
+	public $qsp_form_field = ['y'];
+	public $document_label="Document";
+	public $document_item;
+	public $document = null;
+
+	function init(){
+		parent::init();
+
+		$action = $this->api->stickyGET('action')?:'view';
+		// $this->add('View_Info')->set('QSP=');
+
+		$this->document = $document = $this->add('xepan\base\View_Document',
+							['action'=>$action],
+							null,
+							['view/qsp/master']
+						);
+		$document->setIdField('document_id');
+		$document->setModel($this->qsp_model,$this->qsp_view_field,$this->qsp_form_field);
+
+		$document->form->getElement('discount_amount')->js('change')->_load('xepan-QSIP')->univ()->calculateQSIP();
+		
+		if($this->qsp_model->loaded()){
+			$this->document_item=$qsp_details = $document->addMany('Items',
+										null,
+										'item_info',
+										['view/qsp/details'],
+										'xepan\commerce\Grid_QSP',	
+										'xepan\commerce\CRUD_QSP'
+									);
+			$m = $this->qsp_model->ref('Details');
+			$qsp_details->setModel($m);
+			
+			$qs = $this->add('xepan\commerce\View_QSPDetailJS');
+			if(isset($qsp_details->form)){
+				$form = $qsp_details->form;
+				$tax_field = $form->getElement('taxation_id');
+				$tax_percentage = $form->getElement('tax_percentage');
+
+				if($id=$_GET['tax_id']){
+					$tax_percentage->set(
+						$this->add('xepan\commerce\Model_Taxation')
+							->load($id)
+							->get('percentage')
+							);
+					return;
+				}
+
+
+				$tax_field->js('change',$form->js()->atk4_form(
+								'reloadField','tax_percentage',
+								[
+									$this->app->url(),
+									'tax_id'=>$tax_field->js()->val()
+								]
+								));
+			}
+			
+
+
+
+		}
+	}
+
+}
