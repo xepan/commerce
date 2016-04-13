e8955314c9169a1b551ca4681cdc5a8dae753575
sinharakesh <rksinha.btech@gmail.com>
2016-04-13 17:50:53 +0530

    search

--- lib/Tool/Search.php
+++ lib/Tool/Search.php
@@ -2,11 +2,21 @@
 namespace xepan\commerce;
 
 class Tool_Search extends \xepan\cms\View_Tool{
-	public $option = [];
+	public $options = ['form_layout'=>'view/tool/form/search'];
 	function init(){
 		parent::init();
 
 		$form = $this->add('Form',null,null,['form/empty']);
-		$form->addField('line','content');
+		$form->setLayout($this->options['form_layout']);
+		$form_field = $form->addField('line','search');
+
+		$btn = $form->layout->add('Button',null,'search_button')
+					->set(
+							array(
+									$this->options['form-btn-label']?:"search",
+									'icon'=>'search'
+								)
+						)->js('click',$form->js()->submit());
+
 	}
 }
