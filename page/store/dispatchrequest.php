1b681088c003270b9d82f5ccf56c3832b09f0892
vijay mali <guddavijay@gmail.com>
2016-04-13 11:40:50 +0530

    dispatch request or dispatch item page marged

--- page/store/dispatchrequest.php
+++ page/store/dispatchrequest.php
@@ -3,11 +3,11 @@
 namespace xepan\commerce;
 
 class page_store_dispatchrequest extends \Page{
-	public $title="Dispatch Request";
+	public $title="Dispatch Request Item";
 	function init(){
 		parent::init();
 
-		$dispatch=$this->add('xepan\commerce\Model_Store_Transaction');	
+		$dispatch=$this->add('xepan\commerce\Model_Store_DispatchRequest');	
 		$dispatch->addCondition('type','Dispatch');
 		$c=$this->add('xepan\hr\CRUD',null,null,['view/store/dispatch-request-grid']);
 		$c->setModel($dispatch);
