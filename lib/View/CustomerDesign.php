4fe494bec0c039d852b254bd55cbf5ca7cf4151a
sinharakesh <rksinha.btech@gmail.com>
2016-04-20 18:54:35 +0530

    customerdesign duplicate item and design it's template or design

--- lib/View/CustomerDesign.php
+++ lib/View/CustomerDesign.php
@@ -77,10 +77,10 @@ class View_CustomerDesign extends \View {
 				$g->current_row['template_edit'] = $template_edit_url;
 						
 				//designs
-				$design_thumb_url = $this->api->url('xepan_commerce_designer_thumbnail',['item_member_design_id'=>$g->model->id]);
+				$design_thumb_url = $this->api->url('xepan_commerce_designer_thumbnail',['item_member_design_id'=>$g->model->id,'width'=>100]);
 				$g->current_row['design_thumb_url'] = $design_thumb_url;
 				
-				$design_edit_url = $this->app->url($designer_page,array('xsnb_design_item_id'=>'not-available','xsnb_design_template'=>'false','item_member_design_id'=>$g->model->id));
+				$design_edit_url = $this->app->url($designer_page,array('xsnb_design_item_id'=>'not-available','xsnb_design_template'=>'false','item_member_design'=>$g->model->id));
 				$g->current_row['design_edit'] = $design_edit_url;
 			});
 
