06c3f3701ea6f203365b2917ccec6a6d8341e4bb
dkanojia <dkanojia93@gmail.com>
2016-04-23 18:52:50 +0530

    generate pdf function added

--- lib/View/QSP.php
+++ lib/View/QSP.php
@@ -1,5 +1,5 @@
 <?php
- namespace xepan\commerce;
+namespace xepan\commerce;
 class View_QSP extends \View{
 
 	public $qsp_model;
@@ -16,23 +16,24 @@ class View_QSP extends \View{
 		// $this->add('View_Info')->set('QSP=');
 
 		$this->document = $document = $this->add('xepan\base\View_Document',
-							['action'=>$action],
-							null,
-							['view/qsp/master']
-						);
+			['action'=>$action],
+			null,
+			['view/qsp/master']
+			);
 		$document->setIdField('document_id');
 		$document->setModel($this->qsp_model,$this->qsp_view_field,$this->qsp_form_field);
 
 		$document->form->getElement('discount_amount')->js('change')->_load('xepan-QSIP')->univ()->calculateQSIP();
 		
+
 		if($this->qsp_model->loaded()){
 			$this->document_item=$qsp_details = $document->addMany('Items',
-										null,
-										'item_info',
-										['view/qsp/details'],
-										'xepan\commerce\Grid_QSP',	
-										'xepan\commerce\CRUD_QSP'
-									);
+				null,
+				'item_info',
+				['view/qsp/details'],
+				'xepan\commerce\Grid_QSP',	
+				'xepan\commerce\CRUD_QSP'
+				);
 			$m = $this->qsp_model->ref('Details');
 			$qsp_details->setModel($m);
 			
@@ -45,33 +46,38 @@ class View_QSP extends \View{
 				if($id=$_GET['tax_id']){
 					$tax_percentage->set(
 						$this->add('xepan\commerce\Model_Taxation')
-							->load($id)
-							->get('percentage')
-							);
+						->load($id)
+						->get('percentage')
+						);
 					return;
 				}
 
 
 				$tax_field->js('change',$form->js()->atk4_form(
-								'reloadField','tax_percentage',
-								[
-									$this->app->url(),
-									'tax_id'=>$tax_field->js()->val()
-								]
-								));
+					'reloadField','tax_percentage',
+					[
+					$this->app->url(),
+					'tax_id'=>$tax_field->js()->val()
+					]
+					));
+			}
+
+			if($_GET['generatePdf']){
+				$this->generatePDF();
+				return;
 			}
 		}
-
-		$this->on('click','.xepan-print-qsp',function($js,$data){
-
-				$this->printqsp();
-
-				return $this->js()->univ()->successMessage('Done');
-			});
+		
+		$this->on('click','.xepan-print-qsp',function($js,$data)
+		{
+			return $js->univ()->location($this->api->url(null,['generatePdf'=>1]));
+		});
+		
 	}
 
-	function printqsp(){
-		// create new PDF document
+	function generatePDF(){
+
+
 		$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 		// set document information
 		$pdf->SetCreator(PDF_CREATOR);
@@ -89,15 +95,18 @@ class View_QSP extends \View{
 		// add a page
 		$pdf->AddPage();
 
-		// writeHTML($html, $ln=true, $fill=false, $reseth=false, $cell=false, $align='')
-		// writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true)
-
-
 		// create some HTML content
-		// $html = file_get_contents('../text.txt');
-		$html = $this->document->getHtml();
+		$html = "<style>".file_get_contents('templates/css/bootstrap/bootstrap.min.css');
+		// $html .= file_get_contents('templates/css/compiled/theme_styles.css');
+		// $html .= file_get_contents('templates/css/xepan.css');
+		$html .="</style>";
+
+		$html .= $this->document->getHtml();
+
+		// $html = '<table border="1"><tr><td style="width:30%;">Rakeh</td><td style="width:70%;">Rakesh 33</td></tr></table>';
+
 		// output the HTML content
-		$pdf->writeHTML($html, true, false, true, false, '');
+		$pdf->writeHTML($html, false, false, true, false, '');
 		// set default form properties
 		$pdf->setFormDefaultProp(array('lineWidth'=>1, 'borderStyle'=>'solid', 'fillColor'=>array(255, 255, 200), 'strokeColor'=>array(255, 128, 128)));
 		// reset pointer to the last page
--- page/test.php
+++ page/test.php
@@ -7,41 +7,43 @@ class page_test extends \Page{
 	function init(){
 		parent::init();
 
+		$this->add('xepan\commerce\View_QSP')->generatePDF();
+
 		// create new PDF document
-		$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-		// set document information
-		$pdf->SetCreator(PDF_CREATOR);
-		$pdf->SetAuthor('Rakesh Sinha');
-		$pdf->SetTitle('PDF Example');
-		$pdf->SetSubject('PDF Subject');
-		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+		// $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		// // set document information
+		// $pdf->SetCreator(PDF_CREATOR);
+		// $pdf->SetAuthor('Rakesh Sinha');
+		// $pdf->SetTitle('PDF Example');
+		// $pdf->SetSubject('PDF Subject');
+		// $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
-		// set default monospaced font
-		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+		// // set default monospaced font
+		// $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-		// set font
-		$pdf->SetFont('dejavusans', '', 10);
+		// // set font
+		// $pdf->SetFont('dejavusans', '', 10);
 
-		// add a page
-		$pdf->AddPage();
+		// // add a page
+		// $pdf->AddPage();
 
-		// writeHTML($html, $ln=true, $fill=false, $reseth=false, $cell=false, $align='')
-		// writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true)
+		// // writeHTML($html, $ln=true, $fill=false, $reseth=false, $cell=false, $align='')
+		// // writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true)
 
 
-		// create some HTML content
-		// $html = file_get_contents('../text.txt');
+		// // create some HTML content
+		// // $html = file_get_contents('../text.txt');
 
-		$html = "<style>".file_get_contents("../bootstrap.min.css")."</style>";
-		$html .= file_get_contents('../text.txt');
-		// output the HTML content
-		$pdf->writeHTML($html, true, false, true, false, '');
-		// set default form properties
-		$pdf->setFormDefaultProp(array('lineWidth'=>1, 'borderStyle'=>'solid', 'fillColor'=>array(255, 255, 200), 'strokeColor'=>array(255, 128, 128)));
-		// reset pointer to the last page
-		$pdf->lastPage();
-		//Close and output PDF document
-		$pdf->Output(null, 'I');
+		// $html = "<style>".file_get_contents("../bootstrap.min.css")."</style>";
+		// $html .= file_get_contents('../text.txt');
+		// // output the HTML content
+		// $pdf->writeHTML($html, true, false, true, false, '');
+		// // set default form properties
+		// $pdf->setFormDefaultProp(array('lineWidth'=>1, 'borderStyle'=>'solid', 'fillColor'=>array(255, 255, 200), 'strokeColor'=>array(255, 128, 128)));
+		// // reset pointer to the last page
+		// $pdf->lastPage();
+		// //Close and output PDF document
+		// $pdf->Output(null, 'I');
 
 	}
 }
