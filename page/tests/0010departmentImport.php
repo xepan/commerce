5b929262c5c243bb60373bba5610b17812ad378d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-30 14:06:41 +0530

    department testing with json file writing

--- page/tests/0010departmentImport.php
+++ page/tests/0010departmentImport.php
@@ -17,7 +17,7 @@ class page_tests_0010departmentImport extends \xepan\base\Page_Tester {
 	
 	public $proper_responses=[
     	'test_checkEmptyRows'=>['department'=>1],
-    	'test_ImportDepartments'=>['Company','']
+    	'test_ImportDepartments'=>['Company','Designing','Offset Printing','Digital Press','Large Format','Screen Printing','Varnish','Lamination','UV','Foil','Cutting','Die Cut','Laser Cut','Binding','Pasting','Frame']
     ];
 
     function init(){
@@ -40,18 +40,32 @@ class page_tests_0010departmentImport extends \xepan\base\Page_Tester {
                         ->where('production_level','>=',1)
                         ->where('production_level','<=',100)
                         ->get();
+        $file_data=[];
         foreach ($old_depts as $dept){
                 $new_dept
                 ->set('name',$dept['name'])
                 ->set('production_level',$dept['production_level'])
                 ->set('is_outsourced',$dept['is_outsourced'])
-                ->saveAndUnload()
+                ->save()
                 ;
+
+                $file_data[$dept['id']] = ['new_id'=>$new_dept->id];
+
+                $new_dept->unload();
         }
+
+        file_put_contents(__DIR__.'/department_mapping.json', json_encode($file_data));
     }
 
     function test_ImportDepartments(){
+        $new_depts = $this->api->db->dsql()->table('department')->get();
 
+        $result = [];
+        foreach ($new_depts as $d) {
+            $result[] = $d['name'];
+        }
+
+        return $result;
     }
 
 }
