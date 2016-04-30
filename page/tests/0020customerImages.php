82b6b8e0ed5d5117b94991bd625e9214316827f6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-30 19:53:15 +0530

    Images imported for designer / customer

--- page/tests/0020customerImages.php
+++ page/tests/0020customerImages.php
@@ -16,7 +16,8 @@ class page_tests_0020customerImages extends \xepan\base\Page_Tester {
 	public $title='Customer Images';
 
 	public $proper_responses=[
-		'test_libraryImageCategory'=>'43'
+		'test_libraryImageCategory'=>'43',
+		'test_designerImagesImport'=>'664'
 	];
 
 	function init(){
@@ -26,42 +27,50 @@ class page_tests_0020customerImages extends \xepan\base\Page_Tester {
         parent::init();
     }
 
-  //   function prepare_libraryImageCategory(){
-  //   	$old_m = $this->pdb->dsql()->table('xshop_image_library_category')
-  //                       ->get();
+    function prepare_libraryImageCategory(){
+    	$old_m = $this->pdb->dsql()->table('xshop_image_library_category')
+                        ->get();
 
-  //       $customer_mapping = $this->add('xepan\commerce\page_tests_init')->getMapping('customer');
+        $customer_mapping = $this->add('xepan\commerce\page_tests_init')->getMapping('customer');
 
-  //       $new_m = $this->add('xepan\commerce\Model_Designer_Image_Category');
-		// $file_data=[];
-  //       foreach ($old_m as $om) {
-  //       	$new_m['contact_id'] = $customer_mapping[$om['member_id']]['new_id'];
-  //       	$new_m['name'] = $om['name'];
-  //       	$new_m->save();
+        $new_m = $this->add('xepan\commerce\Model_Designer_Image_Category');
+		$file_data=[];
+        foreach ($old_m as $om) {
+        	$new_m['contact_id'] = $customer_mapping[$om['member_id']]['new_id'];
+        	$new_m['name'] = $om['name'];
+        	$new_m->save();
 
-  //       	$file_data[$om['id']] = ['new_id'=>$new_m->id];
-  //       	$new_m->unload();
-  //       }
+        	$file_data[$om['id']] = ['new_id'=>$new_m->id];
+        	$new_m->unload();
+        }
 
-  //       file_put_contents(__DIR__.'/image_library_catagory_mapping.json', json_encode($file_data));
-  //   }
+        file_put_contents(__DIR__.'/image_library_catagory_mapping.json', json_encode($file_data));
+    }
 
-  //   function test_libraryImageCategory(){
-  //       return $this->add('xepan\commerce\Model_Designer_Image_Category')->count()->getOne();
+    function test_libraryImageCategory(){
+        return $this->add('xepan\commerce\Model_Designer_Image_Category')->count()->getOne();
 
-  //   }
+    }
   
 
     function prepare_designerImagesImport(){
         $old_m = $this->pdb->dsql()->table('xshop_member_images')
 			        ->get();
+        
+        $image_lib_cat_mapping = $this->add('xepan\commerce\page_tests_init')->getMapping('image_library_catagory');
 
 		$new_m = $this->add('xepan\commerce\Model_Designer_Images');
 
 		foreach ($old_m as $om) {
-			$new_m['image_id'] = $om['image_id']
+			$new_m['image_id'] = $om['image_id'];
+			$new_m['designer_category_id'] = $image_lib_cat_mapping[$om['category_id']]['new_id'];
+			$new_m->saveAndUnload();
 		}
 
+    }
+
+    function test_designerImagesImport(){
+    	return $this->api->db->dsql()->table('designer_images')->del('fields')->field('count(*)')->getOne();
     }
 
 
