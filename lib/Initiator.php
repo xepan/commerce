52446f9d190676d0197291d1ac4ae8da9e22abc2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-05-01 17:39:38 +0530

    More import files

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -99,6 +99,16 @@ class Initiator extends \Controller_Addon {
         $d->sql_templates['delete'] = "delete [table] from  [table] [join] [where]";
         $d->table('item')->where('document.id is null')->join('document',null,'left')->delete();
 
+        // orphan custome field associations
+        $d = $this->app->db->dsql();
+        $d->sql_templates['delete'] = "delete [table] from  [table] [join] [where]";
+        $d->table('customfield_association')->where('item.id is null')->join('item',null,'left')->delete();
+
+        // orphan custome field assos values
+        $d = $this->app->db->dsql();
+        $d->sql_templates['delete'] = "delete [table] from  [table] [join] [where]";
+        $d->table('customfield_association')->where('customfield_generic.id is null')->join('customfield_generic',null,'left')->delete();
+
         $this->app->db->dsql()->table('designer_images')->where('epan_id',null)->delete();
         
 		$this->app->epan=$this->app->new_epan;
