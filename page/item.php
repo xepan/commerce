ab59dc2ccd376382c04e5aaa37b12ae026348bea
vaibhav <vaibhavsharma38@gmail.com>
2016-05-02 20:40:47 +0530

    item side menu done

--- page/item.php
+++ page/item.php
@@ -15,19 +15,43 @@
 			$item->addCondition($condition,true);			
 		}
 		
-		// $model_item=$this->add('xepan\commerce\Model_Item');
-		// $saleable_count = $model_item->addCondition('is_saleable',true);
+		$model_item=$this->add('xepan\commerce\Model_Item');
+		$saleable_count = $model_item->addCondition('is_saleable',true)->count();
+		
+		$model_item1=$this->add('xepan\commerce\Model_Item');
+		$purchasable_count = $model_item1->addCondition('is_purchasable',true)->count();
+		
+		$model_item2=$this->add('xepan\commerce\Model_Item');
+		$productionable_count = $model_item2->addCondition('is_productionable',true)->count();
+		
+		$model_item3=$this->add('xepan\commerce\Model_Item');
+		$allowuploadable_count = $model_item3->addCondition('is_purchasable',true)->count();
+		
+		$model_item4=$this->add('xepan\commerce\Model_Item');
+		$template_count = $model_item4->addCondition('is_template',true)->count();
+		
+		$model_item5=$this->add('xepan\commerce\Model_Item');
+		$designable_count = $model_item5->addCondition('is_designable',true)->count();
+		
+		$model_item6=$this->add('xepan\commerce\Model_Item');
+		$dispatchable_count = $model_item6->addCondition('is_dispatchable',true)->count();
+		
+		$model_item7=$this->add('xepan\commerce\Model_Item');
+		$maintaininventory_count = $model_item7->addCondition('maintain_inventory',true)->count();
+		
+		$model_item8=$this->add('xepan\commerce\Model_Item');
+		$allownewgativestock_count = $model_item8->addCondition('allow_negative_stock',true)->count();
 
 
-		$this->app->side_menu->addItem(['Saleable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_saleable']));
-		$this->app->side_menu->addItem(['Purchasable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_purchasable']));
-		$this->app->side_menu->addItem(['Productionable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_productionable']));
-		$this->app->side_menu->addItem(['AllowUploadable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_allowuploadable']));
-		$this->app->side_menu->addItem(['Template','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_template']));
-		$this->app->side_menu->addItem(['Designable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_designable']));
-		$this->app->side_menu->addItem(['Dispatchable','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_dispatchable']));
-		$this->app->side_menu->addItem(['Maintain Inventory','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'maintain_inventory']));
-		$this->app->side_menu->addItem(['Allow Negative Stock','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'allow_negative_stock']));
+		$this->app->side_menu->addItem(['Saleable','badge'=>[$saleable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_saleable']));
+		$this->app->side_menu->addItem(['Purchasable','badge'=>[$purchasable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_purchasable']));
+		$this->app->side_menu->addItem(['Productionable','badge'=>[$productionable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_productionable']));
+		$this->app->side_menu->addItem(['AllowUploadable','badge'=>[$allowuploadable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_allowuploadable']));
+		$this->app->side_menu->addItem(['Template','badge'=>[$template_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_template']));
+		$this->app->side_menu->addItem(['Designable','badge'=>[$designable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_designable']));
+		$this->app->side_menu->addItem(['Dispatchable','badge'=>[$dispatchable_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'is_dispatchable']));
+		$this->app->side_menu->addItem(['Maintain Inventory','badge'=>[$maintaininventory_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'maintain_inventory']));
+		$this->app->side_menu->addItem(['Allow Negative Stock','badge'=>[$allownewgativestock_count,'swatch'=>' label label-primary label-circle pull-right']],$this->app->url('xepan_commerce_item',['condition'=>'allow_negative_stock']));
 
 		
 		$crud=$this->add('xepan\hr\CRUD',
