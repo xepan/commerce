0ac07e27dd536182b0ff6aa238150da038a95db5
dkanojia <dkanojia93@gmail.com>
2016-05-05 19:21:25 +0530

    tool category bug fixed

--- lib/Tool/Category.php
+++ lib/Tool/Category.php
@@ -17,6 +17,7 @@ class Tool_Category extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 
+		return;
 
 		$categories = $this->add('xepan\commerce\Model_Category');
 		$categories->setOrder('display_sequence','asc');
