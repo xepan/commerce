425871c99b333291205c9900124c5349763d075d
vijay mali <guddavijay@gmail.com>
2016-05-09 15:40:54 +0530

    remove model array field in crud

--- page/store/transaction.php
+++ page/store/transaction.php
@@ -7,7 +7,7 @@ class page_store_transaction extends \xepan\base\Page{
 		parent::init();
 		$transaction=$this->add('xepan\commerce\Model_Store_Transaction');
 		$crud=$this->add('xepan\hr\CRUD',null,null,['view/store/transaction-grid']);
-		$crud->setModel($transaction,['from_warehouse','to_warehouse','related_document_id','document_type']);
+		$crud->setModel($transaction);
 		$crud->grid->addPaginator(10);
 		$crud->grid->addQuickSearch(['name']);
 	}
