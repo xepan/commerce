db5f9df5fc5ea7ce31595f601d1584d0bbebb07e
vaibhav <vaibhavsharma38@gmail.com>
2016-05-09 20:29:31 +0530

    category tool upgraded

--- page/test.php
+++ page/test.php
@@ -2,16 +2,18 @@
 
 namespace xepan\commerce;
 
-class page_test extends \Page{
+class page_test extends \xepan\base\Page{
 	
 	function init(){
 		parent::init();
 
-		$ord = $this->add('xepan\commerce\Model_SalesOrder')
-			->addCondition('contact_id',656);
-			$crud_ord = $this->add('xepan\hr\CRUD',null,null,['view/customer/order/grid']);
-			$crud_ord->setModel($ord);
-			$crud_ord->grid->addQuickSearch(['orders']);
+		// $ord = $this->add('xepan\commerce\Model_SalesOrder')
+		// 	->addCondition('contact_id',656);
+		// 	$crud_ord = $this->add('xepan\hr\CRUD',null,null,['view/customer/order/grid']);
+		// 	$crud_ord->setModel($ord);
+		// 	$crud_ord->grid->addQuickSearch(['orders']);
+
+		$this->add('xepan/commerce/Tool_Categorytest');
 
 		}
 }
