946d38e3b73c14d5f30ca5e40dd0fd751ed0a0a0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-05-22 17:21:08 +0530

    Checkout Tool created

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -68,6 +68,7 @@ class Initiator extends \Controller_Addon {
 		$this->app->exportFrontEndTool('xepan\commerce\Tool_ItemList','commerce');
 		$this->app->exportFrontEndTool('xepan\commerce\Tool_ItemImage','commerce');
 		$this->app->exportFrontEndTool('xepan\commerce\Tool_Item_Detail','commerce');
+		$this->app->exportFrontEndTool('xepan\commerce\Tool_Checkout','commerce');
 		$customer=$this->add('xepan\commerce\Model_Customer');
 		$this->app->addHook('userCreated',[$customer,'createNewCustomer']);
 		return $this;
