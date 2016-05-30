30cf64a9f813e4297b6bb13ff05b398fb9e084ed
sinharakesh <rksinha.btech@gmail.com>
2016-05-30 12:22:36 +0530

    form to wizard converted with multi instance

--- templates/js/tool/formToWizard.js
+++ templates/js/tool/formToWizard.js
@@ -8,65 +8,68 @@
         
         var element = this;
 
+        var form_id = $(element).closest('div.xepan-addtocartbutton-multi-step-wizard').attr('id');
+
         var steps = $(element).find("fieldset");
         var count = steps.size();
         var submmitButtonName = "#" + options.submitButton;
         $(submmitButtonName).hide();
 
         // 2
-        $(element).before("<ul id='steps'></ul>");
+        $(element).before("<ul class=\"steps\" id='steps_"+form_id+"'></ul>");
 
         steps.each(function(i) {
-            $(this).wrap("<div id='step" + i + "'></div>");
-            $(this).append("<p id='step" + i + "commands'></p>");
-
-            // 2
+            $(this).wrap("<div id='step_" + i + "_" + form_id + "'></div>");
+            $(this).append("<p id='step_" + i + "_" + form_id + "commands'></p>");
+            
             var name = $(this).find("legend").html();
-            $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
+            $("#steps_"+form_id).append("<li id='stepDesc_" + i + "_" + form_id + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
 
             if (i == 0) {
-                createNextButton(i);
-                selectStep(i);
+                createNextButton(i,form_id);
+                selectStep(i,form_id);
             }
             else if (i == count - 1) {
-                $("#step" + i).hide();
-                createPrevButton(i);
+                $("#step_" + i + "_" + form_id).hide();
+                createPrevButton(i,form_id);
             }
             else {
-                $("#step" + i).hide();
-                createPrevButton(i);
-                createNextButton(i);
+                $("#step_" + i + "_" + form_id ).hide();
+                createPrevButton(i,form_id);
+                createNextButton(i,form_id);
             }
         });
 
-        function createPrevButton(i) {
-            var stepName = "step" + i;
+        function createPrevButton(i,form_id) {
+            var stepName = "step_" + i + "_" + form_id;
             $("#" + stepName + "commands").append("<a href='#prev' id='" + stepName + "Prev' class='prev'>< Back</a>");
 
             $("#" + stepName + "Prev").bind("click", function(e) {
                 $("#" + stepName).hide();
-                $("#step" + (i - 1)).show();
+                $("#step_" + (i - 1) + "_" + form_id).show();
                 $(submmitButtonName).hide();
-                selectStep(i - 1);
+                selectStep(i - 1, form_id);
             });
         }
 
-        function createNextButton(i) {
-            var stepName = "step" + i;
+        function createNextButton(i,form_id) {
+            var stepName = "step_" + i + "_" + form_id;
+
             $("#" + stepName + "commands").append("<a href='#next' id='" + stepName + "Next' class='next'>Next ></a>");
 
             $("#" + stepName + "Next").bind("click", function(e) {
                 $("#" + stepName).hide();
-                $("#step" + (i + 1)).show();
+                $("#step_" + (i + 1)+"_" +form_id).show();
                 if (i + 2 == count)
                     $(submmitButtonName).show();
-                selectStep(i + 1);
+                selectStep(i + 1,form_id);
             });
         }
 
-        function selectStep(i) {
-            $("#steps li").removeClass("current");
-            $("#stepDesc" + i).addClass("current");
+        function selectStep(i,form_id) {
+            $("#steps_"+form_id+" li").removeClass("current");
+            $("#stepDesc_" + i + "_" +form_id).addClass("current");
+            // console.log($("#stepDesc_" + i + "_" +form_id));
         }
 
     }
