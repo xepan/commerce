4fb4fbe159c50fd9208da7f15714bf0dc6940f36
dkanojia <dkanojia93@gmail.com>
2016-06-02 12:05:38 +0530

    country & state drop down fields added in supplier

--- page/supplierdetail.php
+++ page/supplierdetail.php
@@ -23,49 +23,50 @@ class page_supplierdetail extends \xepan\base\Page {
 		
 		$contact_view = $this->add('xepan\base\View_Contact',['acl'=>"xepan\commerce\Model_Supplier"],'contact_view');
 		$contact_view->setModel($supplier);
-		$d = $this->add('xepan\base\View_Document',['action'=>$action,'id_field_on_reload'=>'contact_id'],'basic_info',['page/supplier/detail','basic_info']);
-		$d->setModel($supplier,['tin_no','address','pan_no','organization','city','state','country','currency','pin_code','remark'],['tin_no','address','pan_no','organization','city','state','country','currency_id','pin_code','remark']);
 
-
-/**
-
-		Orders
-
-*/
-
-			$ord = $this->add('xepan\commerce\Model_PurchaseOrder')
-			->addCondition('contact_id',$supplier->id);
-			$crud_ord = $this->add('xepan\hr\CRUD',
-								['action_page'=>'xepan_commerce_purchaseorderdetail'],
-								'orders',
-								['view/supplier/order/grid']);
-			$crud_ord->setModel($ord);
-			$crud_ord->grid->addQuickSearch(['orders']);
-
-/**
-
-		Invoices
-
-*/
-			$inv = $this->add('xepan\commerce\Model_PurchaseInvoice')
-			->addCondition('contact_id',$supplier->id);
-			$crud_inv = $this->add('xepan\hr\CRUD',
-									['action_page'=>'xepan_commerce_purchaseinvoicedetail'],
-									'invoices',
-									['view/supplier/invoice/grid']);
-			$crud_inv->setModel($inv);
-			$crud_inv->grid->addQuickSearch(['invoices']);
-/*
-	Activity
-
-*/
 		if($supplier->loaded()){
-			$activity_view = $this->add('xepan\base\Grid',null,'activity',['view/activity/activity-grid']);
+			$d = $this->add('xepan\base\View_Document',['action'=>$action,'id_field_on_reload'=>'contact_id'],'basic_info',['page/supplier/detail','basic_info']);
+			$d->setModel($supplier,['tin_no','address','pan_no','organization','city','state','country','currency','pin_code','remark'],['tin_no','address','pan_no','organization','city','state_id','country_id','currency_id','pin_code','remark']);
 
-			$activity=$this->add('xepan\base\Model_Activity');
-			$activity->addCondition('contact_id',$_GET['contact_id']);
-			$activity->tryLoadAny();
-			$activity_view->setModel($activity);
+
+	/**
+
+			Orders
+
+	*/
+
+				$ord = $this->add('xepan\commerce\Model_PurchaseOrder')
+				->addCondition('contact_id',$supplier->id);
+				$crud_ord = $this->add('xepan\hr\CRUD',
+									['action_page'=>'xepan_commerce_purchaseorderdetail'],
+									'orders',
+									['view/supplier/order/grid']);
+				$crud_ord->setModel($ord);
+				$crud_ord->grid->addQuickSearch(['orders']);
+
+	/**
+
+			Invoices
+
+	*/
+				$inv = $this->add('xepan\commerce\Model_PurchaseInvoice')
+				->addCondition('contact_id',$supplier->id);
+				$crud_inv = $this->add('xepan\hr\CRUD',
+										['action_page'=>'xepan_commerce_purchaseinvoicedetail'],
+										'invoices',
+										['view/supplier/invoice/grid']);
+				$crud_inv->setModel($inv);
+				$crud_inv->grid->addQuickSearch(['invoices']);
+	/*
+		Activity
+
+	*/
+				$activity_view = $this->add('xepan\base\Grid',null,'activity',['view/activity/activity-grid']);
+
+				$activity=$this->add('xepan\base\Model_Activity');
+				$activity->addCondition('contact_id',$_GET['contact_id']);
+				$activity->tryLoadAny();
+				$activity_view->setModel($activity);
 		}
 
 
