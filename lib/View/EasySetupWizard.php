ee9bf5a543f2de4dd8b71c5c175e5a38e198e7e3
dkanojia <dkanojia93@gmail.com>
2016-06-06 10:38:08 +0530

    easy setup wizard in working

--- lib/View/EasySetupWizard.php
+++ lib/View/EasySetupWizard.php
@@ -1,25 +1,50 @@
 <?php
 
-
 namespace xepan\commerce;
 
 class View_EasySetupWizard extends \View{
 	function init(){
 		parent::init();
-		if($this->add('xepan\hr\Model_Department')->count()->getOne() <= 1000){
-			$v = $this->add('xepan\base\View_Wizard_Step');
-			$v->setTitle('Lorem ipsum dolor sit amet');
-			$v->setMessage('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut porta massa, sit amet maximus odio.');
-			$v->setHelpURL('#');
-			$v->setAction('I M ATION',$v->js()->reload());
 
-			$v1 = $this->add('xepan\base\View_Wizard_Step');
-			$v1->setTitle('Lorem ipsum dolor sit amet');
-			$v1->setMessage('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut porta massa, sit amet maximus odio.');
-			$v1->setHelpURL('#');
-			$v1->setAction('I M ATION',$v->js()->reload());
-
-
+		if($_GET[$this->name.'_set_tax']){
+			$this->js(true)->univ()->frameURL("Taxation System",$this->app->url('xepan_commerce_tax'));
 		}
+
+		$isDone = false;
+
+		$action = $this->js()->reload([$this->name.'_set_tax'=>1]);
+
+			if($this->add('xepan\commerce\Model_TaxationRule')->count()->getOne() > 0){
+				$isDone = true;
+				$action = $this->js()->univ()->dialogOK("Already have Data",' You have already set taxation rules, visit page ? <a href="'. $this->app->url('xepan_commerce_tax')->getURL().'"> click here to go </a>');
+			}
+
+		$tax_view = $this->add('xepan\base\View_Wizard_Step')
+			->setAddOn('Application - Commerce')
+			->setTitle('Specify The Tax')
+			->setMessage('Specify the tax to particular item/product, and add taxes according to norms of organization')
+			->setHelpURL('#')
+			->setAction('Click Here',$action,$isDone);
+		
+
+		if($_GET[$this->name.'_set_item']){
+			$this->js(true)->univ()->frameURL("Products",$this->app->url('xepan_commerce_item'));
+		}
+
+		$isDone = false;
+		
+		$action = $this->js()->reload([$this->name.'_set_item'=>1]);
+
+			if($this->add('xepan\commerce\Model_Item')->count()->getOne() >0){
+				$isDone = true;
+				$action = $this->js()->univ()->dialogOK("Already have Data",' You have already added item, visit page ? <a href="'. $this->app->url('xepan_commerce_item')->getURL().'"> click here to go </a>');
+			}
+
+		$product_view = $this->add('xepan\base\View_Wizard_Step')
+			->setAddOn('Application - Commerce')
+			->setTitle('Products/Item')
+			->setMessage('Please add first product')
+			->setHelpURL('#')
+			->setAction('Click Here',$action,$isDone);
 	}
 }
