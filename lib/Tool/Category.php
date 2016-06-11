0b61d199a3211e9a3ea7b19a23a609e5dc009559
sinharakesh <rksinha.btech@gmail.com>
2016-06-11 11:30:50 +0530

    category tool custom_template option feature implemented

--- lib/Tool/Category.php
+++ lib/Tool/Category.php
@@ -3,11 +3,21 @@ namespace xepan\commerce;
 
 class Tool_Category extends \xepan\cms\View_Tool{
 	public $options = [
-		'url_page' =>'index'
+		'url_page' =>'index',
+		"custom_template"=>''
 	];
 
 	function init(){
 		parent::init();
+		
+		if($this->options['custom_template']){
+			$path = getcwd()."/websites/".$this->app->current_website_name."/www/view/tool/".$this->options['custom_template'].".html";
+			if(!file_exists($path)){
+				$this->add('View_Warning')->set('template not found');
+				return;	
+			}	
+		}else
+			$this->options['custom_template'] = "categorylister";
 
 		$lister = $this->add('xepan\commerce\View_CategoryLister',['options'=>$this->options]);
 	}
--- lib/View/CategoryLister.php
+++ lib/View/CategoryLister.php
@@ -2,37 +2,51 @@
 namespace xepan\commerce;
 class View_CategoryLister extends \CompleteLister{
 		public $options = [
-		'url_page' =>'indeqqqx'
+		'url_page' =>'index',
+		'custom_template'=>"categorylister"
 	];
 
 	function init(){
 		parent::init();
 
-		$model = $this->setModel('xepan\commerce\Category')
-				->addCondition('parent_category_id',0)
+		$model = $this->add('xepan\commerce\Model_Category');
+		$model->addCondition($model->dsql()->orExpr()->where('parent_category_id',0)->where('parent_category_id',null))
 				->addCondition('status','Active');
-		
+		$this->setModel($model);
 	}
 	
 	function formatRow(){
-		$url = $this->model['custom_link']?$this->model['custom_link']:$this->options['url_page'];
+
+		//calculating url
+		if($this->model['custom_link']){
+			// if custom link contains http or https then redirect to that website
+			$has_https = strpos($this->model['custom_link'], "https");
+			$has_http = strpos($this->model['custom_link'], "http");
+			if($has_http === false or $has_https === false )
+				$url = $this->app->url($this->model['custom_link'],['xsnb_category_id'=>$this->model->id]);
+			else
+				$url = $this->model['custom_link'];
+			$this->current_row_html['url'] = $url;
+		}else{
+			$url = $this->app->url($this->options['url_page'],['xsnb_category_id'=>$this->model->id]);
+			$this->current_row_html['url'] = $url;
+		}
 
 		$sub_cat = $this->add('xepan\commerce\Model_Category',['name'=>'model_child_'.$this->model->id]);
 		$sub_cat->addCondition('parent_category_id',$this->model->id);
 
-		if($sub_cat->count()->getOne() > 0){								
-			$sub_c =$this->add('xepan\commerce\View_CategoryLister',null,'nested_category',['view\tool\categorylister','category_list']);
+		if($sub_cat->count()->getOne() > 0){
+			$sub_c =$this->add('xepan\commerce\View_CategoryLister',['options'=>$this->options],'nested_category',['view\tool\categorylister','category_list']);
 			$sub_c->setModel($sub_cat);
 			$this->current_row_html['nested_category']= $sub_c->getHTML();
 		}else
 			$this->current_row_html['nested_category']= "";
 		
-		$this->current_row_html['url']= $this->app->url($url,['xsnb_category_id'=>$this->model->id]);
 
 		parent::formatRow();
 	}
 
 	function defaultTemplate(){
-		return ['view\tool\categorylister'];
+		return ['view\tool\\'.$this->options['custom_template']];
 	}
 }
