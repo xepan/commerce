178875097b6520045e590e90c826c68494b4fe80
sinharakesh <rksinha.btech@gmail.com>
2016-06-13 11:31:36 +0530

    show only active catgegory

--- page/getcategory.php
+++ page/getcategory.php
@@ -6,7 +6,9 @@ class page_getcategory extends \Page{
 	function init(){
 		parent::init();
 
-		$c = $this->add('xepan\commerce\Model_Category');
+		$c = $this->add('xepan\commerce\Model_Category')
+				->addCondition('status','Active');
+
 		$rows = $c->getRows(['id','name']);
 		$option = "";
 		foreach ($rows as $row) {
