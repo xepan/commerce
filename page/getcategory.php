de8293617d7d55de3d3e365688c1db26c37d3860
sinharakesh <rksinha.btech@gmail.com>
2016-06-13 11:12:25 +0530

    multiselect dropdown implemented on frontend tool options and item catgegory option added

---
+++ page/getcategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace xepan\commerce;
+
+class page_getcategory extends \Page{
+	function init(){
+		parent::init();
+
+		$c = $this->add('xepan\commerce\Model_Category');
+		$rows = $c->getRows(['id','name']);
+		$option = "";
+		foreach ($rows as $row) {
+			$option .= "<option value='".$row['id']."'>".$row['name']."</option>";
+		}
+
+		echo $option;
+		exit;
+	}
+}
