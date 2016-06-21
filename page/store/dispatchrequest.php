8a9e3d9aac408b9c27c8f22e4e86f8a9fdea8639
vijay mali <guddavijay@gmail.com>
2016-06-21 14:21:59 +0530

    add frameurl jobcard no & order no to open details

--- page/store/dispatchrequest.php
+++ page/store/dispatchrequest.php
@@ -12,5 +12,9 @@ class page_store_dispatchrequest extends \xepan\base\Page{
 		$dispatch->setOrder('related_document_id','desc');
 		$c = $this->add('xepan\hr\CRUD',null,null,['view/store/dispatch-request-grid']);
 		$c->setModel($dispatch);
+		if(!$c->isEditing()){
+			$c->grid->js('click')->_selector('.do-dispatch-item-jobcard-view')->univ()->frameURL('Item Jobcard Details',[$this->api->url('xepan_production_jobcarddetail'),'document_id'=>$this->js()->_selectorThis()->closest('[data-jobcard-id]')->data('jobcard-id')]);
+			$c->grid->js('click')->_selector('.do-dispatch-order-item-view')->univ()->frameURL('Order Item Details',[$this->api->url('xepan_commerce_salesorderdetail'),'document_id'=>$this->js()->_selectorThis()->closest('[data-related-document-id]')->data('related-document-id')]);
+		}
 	}
 }
