be8baca2fa1e25bc1e8df31b2f0d4199575021e0
vijay mali <guddavijay@gmail.com>
2016-06-23 13:18:33 +0530

    commerce search tool option added

--- lib/Tool/Search.php
+++ lib/Tool/Search.php
@@ -5,7 +5,10 @@ class Tool_Search extends \xepan\cms\View_Tool{
 	public $options = [
 					'form_layout'=>'view/tool/form/search',
 					'form_layout'=>'view/tool/form/search',
-					'xepan_commerce_search_result_page'=>"index"
+					'xepan_commerce_search_result_page'=>"index",
+					'search-field-label'=>"Type Your Search String",
+					'search-form-btn'=>'0',
+					'search-form-btn-label'=>'Search'
 					];
 
 	function init(){
@@ -16,8 +19,18 @@ class Tool_Search extends \xepan\cms\View_Tool{
 			$search_result_subpage="product";
 		}
 
+
+	   	$label = "";
+		if($this->options['search-field-label'] != "")
+			$label = $this->options['search-field-label'];
+
+	   	
 		$form = $this->add('Form',null,null,['form/empty']);
-		$form_field = $form->addField('line','search');
+		$form_field = $form->addField('line','search',$label);//->setAttr('PlaceHolder',$this->options['search-input-placeholder']);
+	   	
+	   	if($this->options['search-form-btn']){
+	 	  		$form->addSubmit($this->options['search-form-btn-label'] !=""?$this->options['search-form-btn-label']:"Search");
+	   	}
 
 		if($form->isSubmitted()){
 			$form->api->redirect(
