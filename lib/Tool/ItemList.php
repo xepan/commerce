f2d6324b2014280ff4c9b643feddf0ae8c743c7c
vijay mali <guddavijay@gmail.com>
2016-06-30 12:07:54 +0530

    currency value define

--- lib/Tool/ItemList.php
+++ lib/Tool/ItemList.php
@@ -256,6 +256,7 @@ class Tool_ItemList extends \xepan\cms\View_Tool{
 		$v->addHook('formatRow',function($m){
 			$m->current_row_html['company_name']=$this->company_m['company_name'];
 			$m->current_row_html['item_image']=$this->app->pm->base_url.$m->model['first_image'];
+			$m->current_row_html['currency']=$this->app->epan->default_currency->get('name');
 		});
 		$l->current_row_html['micro_data']=$v->getHtml();
 	}
