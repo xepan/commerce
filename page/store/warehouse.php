72b53af6d1e4717a6804d53895b1525576692c64
dkanojia <dkanojia93@gmail.com>
2016-07-14 20:12:12 +0530

    country & state condition added in store warehouse

--- page/store/warehouse.php
+++ page/store/warehouse.php
@@ -9,6 +9,13 @@ class page_store_warehouse extends \xepan\base\Page{
 		$crud->form->setLayout('view\store\form\warehouse');
 		$crud->grid->addPaginator(10);
 		$crud->grid->addQuickSearch(['first_name','last_name']);
+
 		$crud->setModel('xepan\commerce\Store_Warehouse',['first_name','last_name','country_id','state_id','city','address','pin_code','organization']);
+		$store_country = $crud->form->getElement('country_id');
+		$store_state = $crud->form->getElement('state_id');
+		
+		if($this->app->stickyGET('country_id'))
+			$store_state->getModel()->addCondition('country_id',$_GET['country_id'])->setOrder('name','asc');
+			$store_country->js('change',$store_state->js()->reload(null,null,[$this->app->url(null,['cut_object'=>$store_state->name]),'country_id'=>$store_country->js()->val()]));
 	}
 }
