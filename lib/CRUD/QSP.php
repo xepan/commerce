8c486ce567f9b7563174e594ea37c268df5a56e2
dkanojia <dkanojia93@gmail.com>
2016-07-19 18:04:06 +0530

    qsp detail grid changes

--- lib/CRUD/QSP.php
+++ lib/CRUD/QSP.php
@@ -5,6 +5,7 @@ namespace xepan\commerce;
 
 class CRUD_QSP extends \xepan\hr\CRUD{
 	public $grid_class='xepan\commerce\Grid_QSP';
+	public $entity_name='Items';
 
 	
 }
--- lib/Model/QSP/Detail.php
+++ lib/Model/QSP/Detail.php
@@ -19,8 +19,8 @@ class Model_QSP_Detail extends \xepan\base\Model_Table{
 		$this->addField('price')->caption('Rate')->type('money');
 		$this->addField('quantity');
 
-		$this->addField('sale_amount'); // not included tax always
-		$this->addField('original_amount'); //not included tax always
+		// $this->addField('sale_amount'); // not included tax always
+		// $this->addField('original_amount'); //not included tax always
 		$this->addField('shipping_charge'); // not included tax always
 		$this->addField('shipping_duration');
 		$this->addField('express_shipping_charge'); //not included tax always
