33113122c8930aaf104895b3c4714e1c4f970072
vaibhav <vaibhavsharma38@gmail.com>
2016-08-04 12:17:33 +0530

    export attachments facility added for orders

--- page/salesorder.php
+++ page/salesorder.php
@@ -38,6 +38,8 @@
 			$crud->grid->js('click')->_selector('.do-view-frame')->univ()->frameURL('Sales Order Details',[$this->api->url('xepan_commerce_salesorderdetail'),'document_id'=>$this->js()->_selectorThis()->closest('[data-salesorder-id]')->data('id')]);
 			$crud->grid->js('click')->_selector('.do-view-customer-frame')->univ()->frameURL('Customer Details',[$this->api->url('xepan_commerce_customerdetail'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-contact-id]')->data('contact-id')]);
 		}
+
+		
 	}
 
 }  
