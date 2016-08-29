a8beea3f866e4d15112c3937485b4c35f32d8e74
vaibhav <vaibhavsharma38@gmail.com>
2016-08-29 18:07:35 +0530

    store dashboard page added

--- page/store/dashboard.php
+++ page/store/dashboard.php
@@ -5,14 +5,5 @@ class page_store_dashboard extends \xepan\base\Page{
 	public $title="Store Dashboard";
 	function init(){
 		parent::init();
-		$item=$this->add('xepan\commerce\Model_Item');
-		// throw new \Exception($item->ref('StoreTransactionRows')->count()->getOne(), 1);
-		
-		$c=$this->add('xepan\hr\Grid',null,null,['view/store/item-grid']);
-		// $c=$this->add('Grid');
-		$c->addPaginator(5);
-		$c->addQuickSearch(['name']);
-		$c->setModel($item)->setOrder('quantity','asc');
-		// $c->setModel($item);
 	}
 }
