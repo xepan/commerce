1280e2b86365015014da3a72bd1030919e0c19f7
dkanojia <dkanojia93@gmail.com>
2016-12-03 19:43:59 +0530

    modelstore transaction updated

---
+++ page/store/activity/opening.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace xepan\commerce;
+
+class page_store_activity_opening extends \xepan\base\Page{
+	public $title="Dispatch Order Item";
+
+	function init(){
+		parent::init();
+		
+		$form = $this->add('Form');
+		$warehouse_field = $form->addField('dropdown','warehouse');
+		$warehouse_field->setModel('xepan\commerce\Model_Store_Warehouse');
+		$item_field = $form->addField('xepan\commerce\Item','item');
+		$item_field->setModel('xepan\commerce\Item');
+		$form->add('Button')->set('Extra-Info')->setClass('btn btn-primary extra-info');
+		$form->addField('text','extra_info');
+		$form->addField('Number','quantity');
+
+		$form->addSubmit('save');
+		if($form->isSubmitted()){
+			$cf_key = $this->add('xepan\commerce\Model_Item')->load($form['item'])->convertCustomFieldToKey($form['extra_info']);
+			
+			$warehouse = $this->add('xepan\commerce\Model_Store_Warehouse')->load($form['warehouse']);
+			$transaction = $warehouse->newTransaction(null,null,$form['warehouse'],'Opening');
+			$transaction->addItem(null,$form['item'],$form['quantity'],null,$cf_key,'Opening');
+		}
+
+	}
+}
