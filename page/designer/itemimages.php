36268f5b441175df4c774881bb15af4710819952
vaibhav <vaibhavsharma38@gmail.com>
2016-12-08 15:18:18 +0530

    designer tool => login tool showed if user not logged in and tries to upload image

--- page/designer/itemimages.php
+++ page/designer/itemimages.php
@@ -12,12 +12,6 @@ class page_designer_itemimages extends \Page {
       $contact = $this->add('xepan\base\Model_Contact');
       $contact->loadLoggedIn();
 
-      // if(!$contact->loaded()){
-      //   $this->add('xepan\base\Tool_UserPanel',null,'login_panel');
-      //   $this->template->set('image_wrapper',"");
-      //   return;
-      // }
-
       $category_id = $this->app->stickyGET('image_folder_id');
       if(!$category_id ){
         $category = $this->add('xepan\commerce\Model_Designer_Image_Category');
@@ -29,44 +23,51 @@ class page_designer_itemimages extends \Page {
         $category_id = $category->id;
       }
 
-      /******** C A T E G O R Y ********/
-      $cat_model = $folder_wrapper->add('xepan\commerce\Model_Designer_Image_Category')
-                    ->addCondition('is_library',false)
-                    ->addCondition('contact_id',$contact->id);
-      $cat_crud = $folder_wrapper->add('xepan\base\CRUD',['entity_name'=>'Folder'],null,['view\designer\managecategory-grid']);
-      $cat_crud->frame_options=['width'=>500];
-      $cat_crud->setModel($cat_model);
-      $cat_crud->grid->addQuickSearch(['name']);
-      
-      /*********** I M A G E ***********/
-      $image_model = $image_wrapper->add('xepan\commerce\Model_Designer_Images');
-      $image_model->addCondition('contact_id',$contact->id);
-      if($category_id)
-        $image_model->addCondition('designer_category_id',$category_id);
+      if(!$contact->loaded()){
+        $this->add('xepan\base\Tool_UserPanel',null,'tool_user_login');
+        // $this->template->set('image_wrapper',"");
+        $this->template->tryDel('my_image_wrapper');
+        // return;
+      }else{
+        /******** C A T E G O R Y ********/
+        $cat_model = $folder_wrapper->add('xepan\commerce\Model_Designer_Image_Category')
+                      ->addCondition('is_library',false)
+                      ->addCondition('contact_id',$contact->id);
+        $cat_crud = $folder_wrapper->add('xepan\base\CRUD',['entity_name'=>'Folder'],null,['view\designer\managecategory-grid']);
+        $cat_crud->frame_options=['width'=>500];
+        $cat_crud->setModel($cat_model);
+        $cat_crud->grid->addQuickSearch(['name']);
+        
+        /*********** I M A G E ***********/
+        $image_model = $image_wrapper->add('xepan\commerce\Model_Designer_Images');
+        $image_model->addCondition('contact_id',$contact->id);
+        if($category_id)
+          $image_model->addCondition('designer_category_id',$category_id);
 
-      $image_model->setOrder('id','desc');
-      $image_crud = $image_wrapper->add('xepan\base\CRUD',['entity_name'=>'Image','allow_edit'=>false,'grid_options'=>['paginator_class'=>'Paginator']],null,['view/designer/designer-item-grid']);
-      $image_crud->frame_options=['width'=>500];
-      $image_crud->setModel($image_model,['image_id'],['image']);
-      $image_crud->grid->addPaginator(50);
-      // $image_crud->grid->js()->trigger('reload')->univ()->alert('vijkay');
-      $image_crud->grid->js(true)->_load('designer-images-insert')->univ()->makeInsertBtn();
-      
-      // onclick on button call image crud button
-      $this->js('click',$this->js()->find('.xepan-designer-images-addimagebutton button')->click())->_selector('.xepan-designer-images-addimagebutton-spot button');
-      
-      // filter image according to category
-      $img_url = $this->app->url(null,['cut_object'=>$image_wrapper->name]);
-      $cat_url = $this->app->url(null,['cut_object'=>$folder_wrapper->name]);
+        $image_model->setOrder('id','desc');
+        $image_crud = $image_wrapper->add('xepan\base\CRUD',['entity_name'=>'Image','allow_edit'=>false,'grid_options'=>['paginator_class'=>'Paginator']],null,['view/designer/designer-item-grid']);
+        $image_crud->frame_options=['width'=>500];
+        $image_crud->setModel($image_model,['image_id'],['image']);
+        $image_crud->grid->addPaginator(50);
+        // $image_crud->grid->js()->trigger('reload')->univ()->alert('vijkay');
+        $image_crud->grid->js(true)->_load('designer-images-insert')->univ()->makeInsertBtn();
+        
+        // onclick on button call image crud button
+        $this->js('click',$this->js()->find('.xepan-designer-images-addimagebutton button')->click())->_selector('.xepan-designer-images-addimagebutton-spot button');
+        
+        // filter image according to category
+        $img_url = $this->app->url(null,['cut_object'=>$image_wrapper->name]);
+        $cat_url = $this->app->url(null,['cut_object'=>$folder_wrapper->name]);
 
-      // //Jquery For Filter the images
-      $cat_crud->on('click','li',function($js,$data)use($img_url,$cat_crud,$image_crud){
-        return [
-            $cat_crud->js()->find('.list-group-item')->removeClass('image-category-active'),
-            $image_crud->js()->reload(['image_folder_id'=>$data['id']],null,$img_url),
-            $js->addClass('image-category-active'),
-          ] ;
-      });
+        // //Jquery For Filter the images
+        $cat_crud->on('click','li',function($js,$data)use($img_url,$cat_crud,$image_crud){
+          return [
+              $cat_crud->js()->find('.list-group-item')->removeClass('image-category-active'),
+              $image_crud->js()->reload(['image_folder_id'=>$data['id']],null,$img_url),
+              $js->addClass('image-category-active'),
+            ] ;
+        });
+      }
 
 
       /*Library Images section Start*/
