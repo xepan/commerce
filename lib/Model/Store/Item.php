9913f0d97b9b20b23fa50946ec8e3167fc206862
sinharakesh <rksinha.btech@gmail.com>
2017-07-17 13:08:25 +0530

    extend from item for adding sum stocke related expression

---
+++ lib/Model/Store/Item.php
@@ -0,0 +1,16 @@
+<?php
+namespace xepan\commerce;
+
+class Model_Store_Item extends \xepan\commerce\Model_Item{
+
+	public $title_field = 'name_with_detail';
+
+	function init(){
+		parent::init();
+
+		$this->addExpression('name_with_detail')->set(function($m,$q){
+			return $q->expr('CONCAT([0]," :: ",[1]," :: ",IFNULL([2]," "))',[$m->getElement('name'),$m->getElement('sku'),$m->getElement('hsn_sac')]);
+		});
+		
+	}	
+}
