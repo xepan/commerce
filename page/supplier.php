5dea4411419c7b70899e85fc5bf875f73e096021
vijay mali <guddavijay@gmail.com>
2017-08-17 14:31:48 +0530

    customer and supplier name with organization name display on grid

--- page/supplier.php
+++ page/supplier.php
@@ -20,12 +20,19 @@ class page_supplier extends \xepan\base\Page {
 		$supplier=$this->add('xepan\commerce\Model_Supplier');
 		$supplier->add('xepan\commerce\Controller_SideBarStatusFilter');
 		
+		$supplier->addExpression('organization_name_with_name')
+					->set($supplier->dsql()
+						->expr('CONCAT(IFNULL([0],"")," ::[ ",IFNULL([1],"")," ]")',
+							[$supplier->getElement('name'),
+								$supplier->getElement('organization')]))
+					->sortable(true);
 
 		$crud=$this->add('xepan\hr\CRUD',
 						['action_page'=>'xepan_commerce_supplierdetail'],
 						null,
 						['view\supplier\grid']
 					);
+
 
 		$crud->setModel($supplier)->setOrder('created_at','desc');
 		$crud->grid->addPaginator(50);
