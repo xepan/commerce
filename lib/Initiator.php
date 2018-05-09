1c16f1c13e5084887522bcab14d97f532a7fcbc8
mamta dangi <MAMTADANGI911@gmail.com>
2018-05-09 14:19:23 -0700

    tool added tools options added model created

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -39,6 +39,8 @@ class Initiator extends \Controller_Addon {
 			$m->addItem(['Commerce Reports','icon'=>'fa fa-cog fa-spin'],'xepan_commerce_reports_customer');
 			$m->addItem(['Store Reports','icon'=>'fa fa-cog fa-spin'],'xepan_commerce_store_reports_itemstock');
 			$m->addItem(['Configuration','icon'=>'fa fa-cog fa-spin'],'xepan_commerce_customfield');
+			$m->addItem(['Review','icon'=>'fa fa-star'],$this->app->url('xepan_commerce_review'));
+
 
 			$this->app->user_menu->addItem(['My Stocks','icon'=>'fa fa-cog fa-spin'],'xepan_commerce_mystocks');
 			/*Store Top Menu & Items*/
@@ -188,6 +190,7 @@ class Initiator extends \Controller_Addon {
 			$this->app->exportFrontEndTool('xepan\commerce\Tool_Search','Commerce');
 			$this->app->exportFrontEndTool('xepan\commerce\Tool_FreelancerListing','Commerce');
 			$this->app->exportFrontEndTool('xepan\commerce\Tool_FreelancerCategory','Commerce');
+			$this->app->exportFrontEndTool('xepan\commerce\Tool_Review','Commerce');
 		}
 
 		$this->app->addHook('cron_executor',function($app){
