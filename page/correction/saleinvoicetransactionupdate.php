cef340338d715dd628a659bc00e686af5b5ea3ee
sinharakesh <rksinha.btech@gmail.com>
2018-07-14 08:39:41 +0530

    page invoice correction are added

---
+++ page/correction/saleinvoicetransactionupdate.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace xepan\commerce;
+
+class page_correction_saleinvoicetransactionupdate extends \xepan\base\Page {
+	public $title='Sale Invoice Transaction Update';
+
+
+	function init(){
+		parent::init();
+
+		ini_set("memory_limit", "-1");
+		set_time_limit(0);
+
+		$this->invid = $this->app->stickyGET("invid")?:0;
+
+		$b = $this->add('Button')->set('Update All Invoice Transaction');
+		$b->add('VirtualPage')
+		->bindEvent('Updating Invoice Transaction','click')
+		->set(function($page){
+
+			$page->add('View_Console')->set(function($c){
+				$c->out('Collecting Invoice Data');
+
+				$si_model = $this->add('xepan\commerce\Model_SalesInvoice');
+				$si_model->setOrder('id','asc');
+				$si_model->addCondition('status',['Due','Paid']);
+
+				if($siid = $this->app->recall('siid',0)){
+					$si_model->addCondition('id','>=',$siid);
+				}
+
+				if($this->invid)
+					$si_model->addCondition('id',$this->invid);
+
+				$si_model->setLimit(15);
+
+				$c->out('Total Invoice:'.$si_model->count()->getOne());
+				$count = 1;
+				$str = "";
+				foreach ($si_model as $model) {
+					try{
+						$model->save();
+						$str .= $model['document_no'].",";
+
+						if( $count%10 == 0 || $this->invid){
+							$c->out('Invoice Updated: '.$count." ids are: ".$str);
+							$str = "";
+						}
+
+						$count++;
+
+						$this->app->memorize('siid',$model->id);
+					}catch(\Exception $e){
+
+					}
+				}
+
+				$c->out('Complete');
+			});
+
+		});
+
+	}
+}
