5f6d8e0cb88898018e140a4840e6fb661a10d4a9
monikapokra <monikagupta495@gmail.com>
2018-07-17 17:24:44 +0530

    discount form beautified

--- page/discountvoucher.php
+++ page/discountvoucher.php
@@ -9,6 +9,25 @@
 
 		$discount = $this->add('xepan\commerce\Model_DiscountVoucher');
 		$crud = $this->add('xepan\hr\CRUD',null,null,['view/discount/vouchers/grid']);
+		if($crud->form){
+			$form = $crud->form;
+			$form->add('xepan\base\Controller_FLC')
+				->showLables(true)
+				->addContentSpot()
+				->layout([
+						'name~Voucher Number'=>'Discount Voucher~c1~3',
+						'start_date'=>'c2~3',
+						'expire_date'=>'c3~3',
+						'status'=>'c4~3',
+						'on_category_id~On Category'=>'c11~6',
+						'include_sub_category'=>'c12~6',
+						'no_of_person'=>'c11~6~How many person ? (i.e. customer for online purchasing)',
+						'one_user_how_many_time'=>'c12~6~How many time it can be used by one customer during online purchasing ?',
+						'on'=>'c11~6',
+						'based_on'=>'c12~6',
+						'FormButtons~&nbsp'=>'c11~6'
+					]);
+		}
 
 		$crud->setModel($discount);
 
