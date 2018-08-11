2158afd01eb702bd9167f90f27bd8ff3e3507c8a
sinharakesh <rksinha.btech@gmail.com>
2018-08-11 12:29:38 +0530

    updation

--- page/warehousemanagment.php
+++ page/warehousemanagment.php
@@ -4,13 +4,12 @@ class page_warehousemanagment extends \xepan\commerce\page_configurationsidebar{
 	public $title="Store Warehouse";
 	function init(){
 		parent::init();
-
-		$crud = $this->add('xepan\hr\CRUD',null,null,['view/store/warehouse-grid']);
-		$crud->form->setLayout('view\store\form\warehouse');
+		
+		$crud = $this->add('xepan\hr\CRUD');
 		$crud->grid->addPaginator(10);
 		$crud->grid->addQuickSearch(['first_name']);
 
-		$crud->setModel('xepan\commerce\Store_Warehouse',['first_name','country_id','state_id','city','address','pin_code','organization'],['first_name','country','state','city','organization','address','pin_code']);
+		$crud->setModel('xepan\commerce\Store_Warehouse',['first_name','country_id','state_id','city','address','pin_code','organization','branch_id'],['first_name','country','state','city','organization','address','pin_code','branch_id']);
 		$store_country = $crud->form->getElement('country_id');
 		$store_state = $crud->form->getElement('state_id');
 		$store_state->dependsOn($store_country);
