24bf91a887e2680bc8202ec3933d1bb74bcb90ef
sinharakesh <rksinha.btech@gmail.com>
2018-08-20 08:40:28 +0530

    variable name updated

--- page/reports/gst.php
+++ page/reports/gst.php
@@ -13,7 +13,7 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 		$grid = $this->add('xepan\hr\Grid');
 		$m = $this->add('xepan\commerce\Model_QSP_GstDetail');
 		$m->addCondition('qsp_type','SalesInvoice');
-		// $m->setLimit(10);
+		$m->setLimit(10);
 		// $m->addCondition('is_this_document_cancelled','Yes');
 		$m->getElement('created_at')->type('date');
 		$fields = ['created_at','invoice_number','customer','customer_gstin','state_place_of_supply','product_type','description','hsn_sac','quantity','qty_unit','price','discount','tax_percentage','cgst_rate','cgst_amount','sgst_rate','sgst_amount','igst_rate','igst_amount','cess_rate','cess_amount','total_transaction_value','is_this_is_a_bill_of_supply','is_this_is_a_nil_rate_exempt_non_gst_item','is_reverse_charge_applicable','type_of_export','shipping_port_code_export','shipping_bill_number_export','shipping_bill_date_export','has_gst_idt_tds_been_deducted','my_gstin','customer_billing_address','customer_billing_city','customer_billing_state','is_this_document_cancelled','is_customer_composition_dealer','return_filling_period','gstin_of_ecommerce_marketplace','date_of_linked_advance_receipt','voucher_number_of_linked_receipt','adjustment_amount_of_linked_receipt','amount_excluding_tax'];
@@ -24,7 +24,7 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 			$norm_name_rate = $norm_name."_rate";
 			$norm_name_amount = $norm_name."_amount";
 
-			$this->taxation[$m['id']] = [
+			$this->taxation[$mt['id']] = [
 								'percentage'=>$mt['percentage'],
 								'name'=>$norm_name,
 								'sub_tax'=>$mt['sub_tax']
