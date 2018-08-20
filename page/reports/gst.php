5d811011a9ed9385d823b9c0b7d8762418ccdbe1
sinharakesh <rksinha.btech@gmail.com>
2018-08-20 08:30:07 +0530

    export field passed

--- page/reports/gst.php
+++ page/reports/gst.php
@@ -16,18 +16,19 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 		// $m->setLimit(10);
 		// $m->addCondition('is_this_document_cancelled','Yes');
 		$m->getElement('created_at')->type('date');
-		$grid->setModel($m,['created_at','invoice_number','customer','customer_gstin','state_place_of_supply','product_type','description','hsn_sac','quantity','qty_unit','price','discount','tax_percentage','cgst_rate','cgst_amount','sgst_rate','sgst_amount','igst_rate','igst_amount','cess_rate','cess_amount','total_transaction_value','is_this_is_a_bill_of_supply','is_this_is_a_nil_rate_exempt_non_gst_item','is_reverse_charge_applicable','type_of_export','shipping_port_code_export','shipping_bill_number_export','shipping_bill_date_export','has_gst_idt_tds_been_deducted','my_gstin','customer_billing_address','customer_billing_city','customer_billing_state','is_this_document_cancelled','is_customer_composition_dealer','return_filling_period','gstin_of_ecommerce_marketplace','date_of_linked_advance_receipt','voucher_number_of_linked_receipt','adjustment_amount_of_linked_receipt','amount_excluding_tax']);
-		foreach ($this->add('xepan\commerce\Model_Taxation') as $m) {
-			$norm_name = $this->app->normalizeName($m['name']);
+		$fields = ['created_at','invoice_number','customer','customer_gstin','state_place_of_supply','product_type','description','hsn_sac','quantity','qty_unit','price','discount','tax_percentage','cgst_rate','cgst_amount','sgst_rate','sgst_amount','igst_rate','igst_amount','cess_rate','cess_amount','total_transaction_value','is_this_is_a_bill_of_supply','is_this_is_a_nil_rate_exempt_non_gst_item','is_reverse_charge_applicable','type_of_export','shipping_port_code_export','shipping_bill_number_export','shipping_bill_date_export','has_gst_idt_tds_been_deducted','my_gstin','customer_billing_address','customer_billing_city','customer_billing_state','is_this_document_cancelled','is_customer_composition_dealer','return_filling_period','gstin_of_ecommerce_marketplace','date_of_linked_advance_receipt','voucher_number_of_linked_receipt','adjustment_amount_of_linked_receipt','amount_excluding_tax'];
+		$grid->setModel($m,$fields);
+		$expression_array = [];
+		foreach ($this->add('xepan\commerce\Model_Taxation') as $mt) {
+			$norm_name = $this->app->normalizeName($mt['name']);
 			$norm_name_rate = $norm_name."_rate";
 			$norm_name_amount = $norm_name."_amount";
 
 			$this->taxation[$m['id']] = [
-								'percentage'=>$m['percentage'],
+								'percentage'=>$mt['percentage'],
 								'name'=>$norm_name,
-								'sub_tax'=>$m['sub_tax']
+								'sub_tax'=>$mt['sub_tax']
 							];
-
 			$grid->addColumn($norm_name_rate);
 			$grid->addColumn($norm_name_amount);
 		}
@@ -62,7 +63,7 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 		});
 
 		$grid->addFormatter('description','wrap');
-		$grid->add('misc\Export');
+		$grid->add('misc\Export',['export_fields'=>$fields]);
 
 	}
 }
