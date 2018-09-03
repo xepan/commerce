3f6a50b339c8365e748eadea4782e5683f0e36ae
sinharakesh <rksinha.btech@gmail.com>
2018-09-03 16:27:38 +0530

    gst reprt updated

--- page/reports/gst.php
+++ page/reports/gst.php
@@ -9,6 +9,25 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 	function init(){
 		parent::init();
 		
+		$this->js()->_load('table2download');
+
+		$from_date = $this->app->stickyGET('from_date');
+		$to_date = $this->app->stickyGET('to_date');
+		$form = $this->add('Form');
+		$form
+			->add('xepan\base\Controller_FLC')
+			->showLables(true)
+				->makePanelsCoppalsible(true)
+				->layout([
+					'from_date'=>'c1~4',
+					'to_date'=>'c2~4',
+					'FormButtons'=>'c3~4'
+				]);
+
+		$form->addField('DatePicker','from_date');
+		$form->addField('DatePicker','to_date');
+		$form->addSubmit('Generate Report');
+
 
 		$grid = $this->add('xepan\hr\Grid');
 		$m = $this->add('xepan\commerce\Model_QSP_GstDetail');
@@ -16,6 +35,18 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 		// $m->addCondition('is_this_document_cancelled','Yes');
 		$m->getElement('created_at')->type('date');
 		$fields = ['created_at','invoice_number','customer','customer_gstin','state_place_of_supply','product_type','description','hsn_sac','quantity','qty_unit','price','discount','tax_percentage','cgst_rate','cgst_amount','sgst_rate','sgst_amount','igst_rate','igst_amount','cess_rate','cess_amount','total_transaction_value','is_this_is_a_bill_of_supply','is_this_is_a_nil_rate_exempt_non_gst_item','is_reverse_charge_applicable','type_of_export','shipping_port_code_export','shipping_bill_number_export','shipping_bill_date_export','has_gst_idt_tds_been_deducted','my_gstin','customer_billing_address','customer_billing_city','customer_billing_state','is_this_document_cancelled','is_customer_composition_dealer','return_filling_period','gstin_of_ecommerce_marketplace','date_of_linked_advance_receipt','voucher_number_of_linked_receipt','adjustment_amount_of_linked_receipt','amount_excluding_tax'];
+		
+		if($_GET['filter']){
+			if($from_date){
+				$m->addCondition('created_at','>=',$from_date);
+			}
+			if($to_date){
+				$m->addCondition('created_at','<',$this->app->nextDate($to_date));
+			}
+		}else{
+			$m->addCondition('id','-1');
+		}
+
 		$grid->setModel($m,$fields);
 		$expression_array = [];
 		foreach ($this->add('xepan\commerce\Model_Taxation') as $mt) {
@@ -61,8 +92,22 @@ class page_reports_gst extends \xepan\commerce\page_reports_reportsidebar{
 
 		});
 
+		if($form->isSubmitted()){
+			$grid->js()->reload(['filter'=>1,'from_date'=>$form['from_date'],'to_date'=>$form['to_date']])->execute();
+		}
+		
 		$grid->addFormatter('description','wrap');
-		$grid->add('misc\Export',['export_fields'=>$fields]);
+		$options = [
+			'format'=> "csv",
+			'separator'=> ",",
+			'filename'=> "gst_report_from_".$from_date."_to_".$to_date,
+			'linkname'=> "Export CSV",
+			'quotes'=> "\"",
+			'btn_prepand_class'=>'#'.$grid->name." .xepan-filter-form"
+		];
+
+		$grid->js(true,$grid->js()->table_download($options)->_selector('#'.$grid->name." table"));
+		// $grid->add('misc\Export',['export_fields'=>$fields]);
 
 	}
 }
