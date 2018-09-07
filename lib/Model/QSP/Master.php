dcfd251873b03f2a8e602256fb73857c6450a0b8
sinharakesh <rksinha.btech@gmail.com>
2018-09-07 16:10:39 +0530

    createQSPMaster branch id added

--- lib/Model/QSP/Master.php
+++ lib/Model/QSP/Master.php
@@ -898,6 +898,7 @@ class Model_QSP_Master extends \xepan\hr\Model_Document{
 		$master_model['contact_id'] = $master_data['contact_id'];
 		$master_model['currency_id'] = $master_data['currency_id'];
 		$master_model['nominal_id'] = $master_data['nominal_id'];
+		$master_model['branch_id'] = $master_data['branch_id'];
 
 		$master_model['billing_country_id'] = $master_data['billing_country_id'];
 		$master_model['billing_state_id'] = $master_data['billing_state_id'];
