68eb4c764d77e7707228814093d70a8ba1b7c351
dkanojia <dkanojia93@gmail.com>
2018-09-15 15:03:59 +0530

    Testing Forking

---
+++ lib/Tool/TestFork.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace xepan\commerce;
+
+class Tool_TestFork extends \View{
+    public $options = [];
+	function init(){
+		parent::init();
+
+		// Test Forking For Pull and Push.....
+
+	}
+
+}		
