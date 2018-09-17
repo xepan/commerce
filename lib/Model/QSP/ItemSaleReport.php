20fb347d5e45e0c13520ca38d7adfb10b4d7d244
sinharakesh <rksinha.btech@gmail.com>
2018-09-17 09:10:34 +0530

    item sales report

---
+++ lib/Model/QSP/ItemSaleReport.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace xepan\commerce;
+
+class Model_QSP_ItemSaleReport extends \xepan\commerce\Model_Item{
+	public $from_date;
+	public $to_date;
+
+	function init(){
+		parent ::init();
+
+		// $this->getElement('created_at')->caption('Invoice Date');
+		// $this->addExpression('qsp_serial')->set($this->refSQL('qsp_master_id')->fieldQuery('serial'));
+		// $this->addExpression('invoice_number')->set(function($m,$q){
+		// 	return $q->expr('CONCAT(IFNULL([0],"")," ",IFNULL([1],""))',[$this->getElement('qsp_serial'),$this->getElement('qsp_master')]);
+		// });
+		// $this->addExpression('is_this_document_cancelled')->set(function($m,$q){
+		// 	return $q->expr('IF([0]="Canceled","Yes","No")',[$m->getElement("qsp_status")]);
+		// });
+		// $this->addExpression('total_transaction_value')->set($this->refSQL('qsp_master_id')->fieldQuery('net_amount'));
+		
+		$this->addExpression('total_sales_invoice')->set(function($m,$q){
+			$qsp_details = $m->add('xepan\commerce\Model_QSP_Detail',['table_alias'=>'total_sales_inv']);
+			$qsp_details->addCondition('qsp_type','SalesInvoice');
+			$qsp_details->addCondition('item_id',$q->getField('id'));
+			if($this->from_date)
+				$qsp_details->addCondition('created_at','>=',$this->from_date);
+			if($this->to_date)
+				$qsp_details->addCondition('created_at','<',$this->app->nextDate($this->to_date));
+
+			return $qsp_details->count();
+		});
+
+		$this->addExpression('total_sales_qty')->set(function($m,$q){
+			$qsp_details = $m->add('xepan\commerce\Model_QSP_Detail',['table_alias'=>'total_sales_qty']);
+			$qsp_details->addCondition('qsp_type','SalesInvoice');
+			$qsp_details->addCondition('item_id',$q->getField('id'));
+			if($this->from_date)
+				$qsp_details->addCondition('created_at','>=',$this->from_date);
+			if($this->to_date)
+				$qsp_details->addCondition('created_at','<',$this->app->nextDate($this->to_date));
+
+			return $qsp_details->_dsql()->del('fields')->field($q->expr('SUM([0])',[$qsp_details->getElement('effective_qty')]));
+		});
+
+		$this->addExpression('total_sales_amount')->set(function($m,$q){
+			$qsp_details = $m->add('xepan\commerce\Model_QSP_Detail',['table_alias'=>'total_sales_amount']);
+			$qsp_details->addCondition('qsp_type','SalesInvoice');
+			$qsp_details->addCondition('item_id',$q->getField('id'));
+			if($this->from_date)
+				$qsp_details->addCondition('created_at','>=',$this->from_date);
+			if($this->to_date)
+				$qsp_details->addCondition('created_at','<',$this->app->nextDate($this->to_date));
+
+			return $qsp_details->sum('total_amount');
+		});
+		$this->addExpression('total_sales_amount_excluding_tax')->set(function($m,$q){
+			$qsp_details = $m->add('xepan\commerce\Model_QSP_Detail',['table_alias'=>'total_sales_ex_tax_amount']);
+			$qsp_details->addCondition('qsp_type','SalesInvoice');
+			$qsp_details->addCondition('item_id',$q->getField('id'));
+			if($this->from_date)
+				$qsp_details->addCondition('created_at','>=',$this->from_date);
+			if($this->to_date)
+				$qsp_details->addCondition('created_at','<',$this->app->nextDate($this->to_date));
+			return $qsp_details->sum('amount_excluding_tax');
+		});
+		$this->addExpression('total_tax_amount')->set(function($m,$q){
+			$qsp_details = $m->add('xepan\commerce\Model_QSP_Detail',['table_alias'=>'total_sales_amount']);
+			$qsp_details->addCondition('qsp_type','SalesInvoice');
+			$qsp_details->addCondition('item_id',$q->getField('id'));
+			if($this->from_date)
+				$qsp_details->addCondition('created_at','>=',$this->from_date);
+			if($this->to_date)
+				$qsp_details->addCondition('created_at','<',$this->app->nextDate($this->to_date));
+			
+			return $qsp_details->sum('tax_amount');
+		});
+
+
+
+
+	}
+}
---
+++ page/reports/itemsalereport.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace xepan\commerce;
+
+class page_reports_itemsalereport extends \xepan\commerce\page_reports_reportsidebar{
+	public $title = 'Item Sales Report';
+	public $taxation = [];
+
+	function init(){
+		parent::init();
+		
+		$this->js()->_load('table2download');
+
+		$from_date = $this->app->stickyGET('from_date');
+		$to_date = $this->app->stickyGET('to_date');
+
+		$form = $this->add('Form');
+		$form
+			->add('xepan\base\Controller_FLC')
+			->showLables(true)
+				->makePanelsCoppalsible(true)
+				->layout([
+					'from_date'=>'c1~4',
+					'to_date'=>'c2~4',
+					'FormButtons'=>'c3~4'
+				]);
+
+		$form->addField('DatePicker','from_date');
+		$form->addField('DatePicker','to_date');
+		$form->addSubmit('Generate Report');
+
+
+		$grid = $this->add('xepan\hr\Grid');
+		$m = $this->add('xepan\commerce\Model_QSP_ItemSaleReport',['from_date'=>$from_date,'to_date'=>$to_date]);
+		// $m->addCondition('qsp_type','SalesInvoice');
+		// $m->addCondition([['qsp_status','Paid'],['qsp_status','Due']]);
+		// $m->addCondition('is_this_document_cancelled','Yes');
+		$m->setOrder('total_sales','desc');
+		$fields = ['name','sku','nominal','total_sales_invoice','total_sales_qty','total_sales_amount_excluding_tax','total_tax_amount','total_sales_amount'];
+		
+		// if($_GET['filter']){
+		// 	if($from_date){
+		// 		$m->addCondition('created_at','>=',$from_date);
+		// 	}
+		// 	if($to_date){
+		// 		$m->addCondition('created_at','<',$this->app->nextDate($to_date));
+		// 	}
+		// }else{
+		// 	$m->addCondition('id','-1');
+		// }
+
+		$grid->setModel($m,$fields);
+		$expression_array = [];
+		// foreach ($this->add('xepan\commerce\Model_Taxation') as $mt) {
+		// 	$norm_name = $this->app->normalizeName($mt['name']);
+		// 	$norm_name_rate = $norm_name."_rate";
+		// 	$norm_name_amount = $norm_name."_amount";
+
+		// 	$this->taxation[$mt['id']] = [
+		// 						'percentage'=>$mt['percentage'],
+		// 						'name'=>$norm_name,
+		// 						'sub_tax'=>$mt['sub_tax']
+		// 					];
+		// 	$grid->addColumn($norm_name_rate);
+		// 	$grid->addColumn($norm_name_amount);
+		// }
+
+
+		// $grid->addHook('formatRow',function($g){
+		// 	$g->current_row['description'] = nl2br(strip_tags($g->model['description']));
+		// 	$tax_id = $g->model['taxation_id'];
+
+		// 	if(isset($this->taxation[$tax_id])){
+		// 		$tax_array = $this->taxation[$tax_id];
+		// 		$norm_name = $tax_array['name'];
+		// 		$norm_name_rate = $norm_name."_rate";
+		// 		$norm_name_amount = $norm_name."_amount";
+
+		// 		$g->current_row[$norm_name_rate] = $tax_array['percentage'];
+		// 		$g->current_row[$norm_name_amount] = ($tax_array['percentage'] * $g->model['amount_excluding_tax'])/100;
+
+		// 		if($sub_tax = $tax_array['sub_tax']){
+		// 			foreach (explode(",", $sub_tax) as $subtax) {
+		// 			 	$st_array = explode("-",$subtax);
+		// 				$subtax_array = $this->taxation[$st_array[0]];
+		// 				$subtax_norm_name = $subtax_array['name'];
+		// 				$subtax_norm_name_rate = $subtax_norm_name."_rate";
+		// 				$subtax_norm_name_amount = $subtax_norm_name."_amount";
+						
+		// 				$g->current_row[$subtax_norm_name_rate] = $subtax_array['percentage'];
+		// 				$g->current_row[$subtax_norm_name_amount] = ($subtax_array['percentage'] * $g->model['amount_excluding_tax'])/100;
+		// 			}
+		// 		}
+		// 	}
+
+		// });
+
+		if($form->isSubmitted()){
+			$grid->js()->reload(['filter'=>1,'from_date'=>$form['from_date'],'to_date'=>$form['to_date']])->execute();
+		}
+				
+		$options = [
+			'format'=> "csv",
+			'separator'=> ",",
+			'filename'=> "item_sales_report_from_".$from_date."_to_".$to_date,
+			'linkname'=> "Export CSV",
+			'quotes'=> "\"",
+			'btn_prepand_class'=>'#'.$grid->name." .xepan-filter-form"
+		];
+
+		$grid->js(true,$grid->js()->table_download($options)->_selector('#'.$grid->name." table"));
+
+	}
+}
